4803f03b87f24c062371771f95782bde6f6ea9c8
Taapat <taapat@gmail.com>
2013-10-13 12:07:52 +0300

    update target-gcc to 4.7.3-129

--- make/bare-os.mk
+++ make/bare-os.mk
@@ -215,7 +215,7 @@ FILES_libstdc++-dev = \
 /usr/lib/*.so*
 
 LIBGCC := libgcc
-GCC_VERSION := 4.7.3-128
+GCC_VERSION := 4.7.3-129
 GCC_SPEC := stm-target-$(GCC).spec
 GCC_SPEC_PATCH := $(GCC_SPEC).$(GCC_VERSION).diff
 GCC_PATCHES := stm-target-$(GCC).$(GCC_VERSION).diff
