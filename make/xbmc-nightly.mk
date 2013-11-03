a8fe52a8fa83a1da4aec22ade0f10b9699411435
schpuntik <schpuntik@freenet.de>
2013-11-03 21:21:10 +0100

    xbmc upgrade to 12.2 Frodo

--- make/xbmc-nightly.mk
+++ make/xbmc-nightly.mk
@@ -1,11 +1,12 @@
-# tuxbox/enigma2
+# tuxbox/xbmc
 BEGIN[[
-xbmc_nightly
+xbmc
   git
-  $(appsdir)/{PN}
+  {PN}-nightly
 ifdef ENABLE_XBD0
-  git://github.com/xbmc/xbmc.git
+  git://github.com/xbmc/xbmc.git:r=12.2-Frodo
   patch:file://xbmc-nightly.0.diff
+  patch:file://xbmc-texture.patch
 endif
 
 ifdef ENABLE_XBD1
@@ -13,40 +14,92 @@ ifdef ENABLE_XBD1
   patch:file://xbmc-nightly.1.diff
 endif
 
-ifdef ENABLE_XBD2
-  git://github.com/xbmc/xbmc.git:r=327710767d2257dad27e3885effba1d49d4557f0
-  patch:file://xbmc-nightly.2.diff
-endif
-
-ifdef ENABLE_XBD3
-  git://github.com/xbmc/xbmc.git:r=12840c28d8fbfd71c26be798ff6b13828b05b168
-  patch:file://xbmc-nightly.3.diff
-endif
-
-ifdef ENABLE_XBD4
-  git://github.com/xbmc/xbmc.git:r=e292b1147bd89a7e53742e3e5039b9a906a3b1d0
-  patch:file://xbmc-nightly.4.diff
-endif
 ;
 ]]END
 
-$(DEPDIR)/xbmc-nightly.do_prepare: $(DEPENDS_xbmc_nightly)
-	$(PREPARE_xbmc_nightly)
+DESCRIPTION_xbmc = xbmc
+BDEPENDS_xbmc = libstgles \
+			libass \
+			libmpeg2 \
+			libmad \
+			libjpeg \
+			libsamplerate \
+			libogg \
+			libvorbis \
+			libmodplug \
+			curl \
+			libflac \
+			bzip2 \
+			tiff \
+			lzo \
+			libz \
+			fontconfig \
+			libfribidi \
+			freetype \
+			jasper \
+			sqlite \
+			libpng \
+			libpcre \
+			libcdio \
+			yajl \
+			libmicrohttpd \
+			tinyxml \
+			python \
+			gstreamer \
+			gst_plugins_dvbmediasink \
+			expat \
+			sdparm \
+			lirc \
+			libnfs \
+			driver-ptinp \
+			taglib \
+			directfb \
+			tvheadend
+PKGR_xbmc =r1
+SRC_URI_xbmc = git://github.com/xbmc/xbmc.git
+DESCRIPTION_xbmc_core = "xbmc core"
+PACKAGES_xbmc = xbmc_core xbmc_addons xbmc_language
+FILES_xbmc_core = /usr/bin/xbmc \
+		/usr/lib/xbmc/* \
+		/usr/share/icons/* \
+		/usr/share/xbmc/media/* \
+		/usr/share/xbmc/sounds/* \
+		/usr/share/xbmc/system/* \
+		/usr/share/xbmc/userdata/* \
+		/usr/share/xbmc/FEH.py
+
+DESCRIPTION_xbmc_addons = "xbmc addons"
+FILES_xbmc_addons = \
+		/usr/share/xbmc/addons/library.xbmc.gui/* \
+		/usr/share/xbmc/addons/library.xbmc.pvr/* \
+		/usr/share/xbmc/addons/library.xbmc.addon/* \
+		/usr/share/xbmc/addons/repository.xbmc.org/* \
+		/usr/share/xbmc/addons/xbmc.metadata/* \
+		/usr/share/xbmc/addons/xbmc.addon/* \
+		/usr/share/xbmc/addons/xbmc.core/* \
+		/usr/share/xbmc/addons/xbmc.gui/* \
+		/usr/share/xbmc/addons/xbmc.json/* \
+		/usr/share/xbmc/addons/xbmc.pvr/* \
+		/usr/share/xbmc/addons/webinterface.default/* \
+		/usr/share/xbmc/addons/skin.confluence/*
+
+DESCRIPTION_xbmc_language = "xbmc language files"
+FILES_xbmc_language = \
+		/usr/share/xbmc/language/Russian/* \
+		/usr/share/xbmc/language/English/* \
+		/usr/share/xbmc/language/German/*
+
+$(DEPDIR)/xbmc-nightly.do_prepare: bootstrap libboost $(DEPENDS_xbmc)
+	$(PREPARE_xbmc)
 	touch $@
 
-$(DIR_xbmc_nightly)/config.status: bootstrap libboost directfb libstgles libass libmpeg2 libmad jpeg libsamplerate libogg libvorbis libmodplug curl libflac bzip2 tiff lzo libz fontconfig libfribidi freetype sqlite libpng libpcre libcdio jasper yajl libmicrohttpd tinyxml python gstreamer gst_plugins_dvbmediasink expat sdparm lirc libnfs driver-ptinp
-	cd $(DIR_xbmc_nightly) && \
+$(DIR_xbmc)/config.status: $(DEPDIR)/xbmc-nightly.do_prepare
+	cd $(DIR_xbmc) && \
 		$(BUILDENV) \
 		./bootstrap && \
 		./configure \
 			--host=$(target) \
 			--prefix=/usr \
-			PKG_CONFIG_SYSROOT_DIR=$(targetprefix) \
-			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
-			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
-			PYTHON_SITE_PKG=$(targetprefix)/usr/lib/python$PYTHON_VERSION/site-packages \
-			PYTHON_CPPFLAGS=-I$(targetprefix)/usr/include/python$PYTHON_VERSION \
-			PY_PATH=$(targetprefix)/usr \
 			SWIG_EXE=none \
 			JRE_EXE=none \
 			--disable-gl \
@@ -78,32 +131,21 @@ $(DIR_xbmc_nightly)/config.status: bootstrap libboost directfb libstgles libass 
 			--enable-dvdplayer \
 			--disable-pulse \
 			--disable-alsa \
-			--disable-ssh
+			--disable-ssh \
+			PYTHON_CPPFLAGS=-I$(targetprefix)/usr/include/python$(PYTHON_VERSION) \
+			PY_PATH=$(targetprefix)/usr 
 
-$(DEPDIR)/xbmc-nightly.do_compile: $(DIR_xbmc_nightly)/config.status
-	cd $(DIR_xbmc_nightly) && \
+$(DEPDIR)/xbmc-nightly.do_compile: $(DIR_xbmc)/config.status
+	cd $(DIR_xbmc) && \
 		$(MAKE) all
 	touch $@
-DESCRIPTION_xbmc_nightly = xbmc
-PKGR_xbmc_nightly =r1
-SRC_URI_xbmc = git://github.com/xbmc/xbmc.git
-FILES_xbmc_nightly = /usr/lib/xbmc/* \
-		     /usr/share/applications/* \
-		     /usr/share/icons/* \
-		     /usr/share/xbmc/language/Russian \
-		     /usr/share/xbmc/language/German \
-		     /usr/share/xbmc/media/* \
-		     /usr/share/xbmc/sounds/* \
-		     /usr/share/xbmc/system/* \
-		     /usr/share/xbmc/userdata/* \
-		     /usr/share/xbmc/FEH.py
 
 
 $(DEPDIR)/xbmc-nightly: xbmc-nightly.do_prepare xbmc-nightly.do_compile
-	$(call parent_pk,xbmc_nightly)
+	$(call parent_pk,xbmc)
 	$(start_build)
 	$(get_git_version)
-	$(MAKE) -C $(DIR_xbmc_nightly) install DESTDIR=$(PKDIR)
+	$(MAKE) -C $(DIR_xbmc) install DESTDIR=$(PKDIR)
 	if [ -e $(PKDIR)/usr/lib/xbmc/xbmc.bin ]; then \
 		$(target)-strip $(PKDIR)/usr/lib/xbmc/xbmc.bin; \
 	fi
@@ -119,5 +161,4 @@ xbmc-nightly-distclean:
 	rm -f $(DEPDIR)/xbmc-nightly
 	rm -f $(DEPDIR)/xbmc-nightly.do_compile
 	rm -f $(DEPDIR)/xbmc-nightly.do_prepare
-	rm -rf $(DIR_xbmc_nightly)
-
+	rm -rf $(DIR_xbmc)
