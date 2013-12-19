0eac52e0cfa570e4d1cf34fbfe3eba598e019609
Taapat <taapat@gmail.com>
2013-12-19 23:19:08 +0200

    Add skin dTV-HD-Reloaded from Taapat git

--- make/externe-skins.mk
+++ make/externe-skins.mk
@@ -117,3 +117,32 @@ e2skin-plihd-distclean:
 	rm -f $(DEPDIR)/e2skin-plihd.do_prepare
 	rm -rf $(DIR_e2skin_plihd)
 
+#
+# skin-dTV-HD-Reloaded
+#
+BEGIN[[
+e2skin_dTVHDReloaded
+  git
+  {PN}
+  git://github.com/Taapat/skin-dTV-HD-Reloaded.git:b=master
+;
+]]END
+
+DESCRIPTION_e2skin_dTVHDReloaded := SD skin dTV-HD-Reloaded from Taapat
+PKGR_e2skin_dTVHDReloaded = r0
+
+$(DEPDIR)/e2skin-dTVHDReloaded.do_prepare: $(DEPENDS_e2skin_dTVHDReloaded)
+	$(PREPARE_e2skin_dTVHDReloaded)
+	touch $@
+
+$(DEPDIR)/e2skin-dTVHDReloaded: e2skin-dTVHDReloaded.do_prepare
+	$(start_build)
+	cd $(DIR_e2skin_dTVHDReloaded) && \
+		cp -a usr $(PKDIR)
+	$(toflash_build)
+	touch $@
+
+e2skin-dTVHDReloaded-distclean:
+	rm -f $(DEPDIR)/e2skin-dTVHDReloaded.do_prepare
+	rm -rf $(DIR_e2skin_dTVHDReloaded)
+
