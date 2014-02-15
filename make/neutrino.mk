f4e2b19958d0312396165c6249ee860e421b41fb
schpuntik <schpuntik@freenet.de>
2014-02-15 16:59:08 +0100

    neutrino  first step

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -1,88 +1,60 @@
 #
-# Makefile to build NEUTRINO
+# NEUTRINO HD
 #
-
-$(targetprefix)/var/etc/.version:
-	echo "imagename=Neutrino-HD" > $@
-	echo "homepage=http://gitorious.org/open-duckbox-project-sh4" >> $@
-	echo "creator=`id -un`" >> $@
-	echo "docs=http://gitorious.org/open-duckbox-project-sh4/pages/Home" >> $@
-	echo "forum=http://gitorious.org/open-duckbox-project-sh4" >> $@
-	echo "version=0100`date +%Y%m%d%H%M`" >> $@
-	echo "git =`git describe`" >> $@
-
-N_CPPFLAGS =-DNEW_LIBCURL
-
-N_CONFIG_OPTS = --enable-silent-rules
-
 #
-# NEUTRINO BETA
-#
-$(DEPDIR)/neutrino-beta.do_prepare:
-	svn co http://109.75.98.228/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta/ --username coolstream --password coolstream $(appsdir)/neutrino-beta
-	rm -rf $(appsdir)/neutrino-beta/lib/libcoolstream/*.*
-	cp -ra $(appsdir)/neutrino-beta $(appsdir)/neutrino-beta.org
-	cd $(appsdir)/neutrino-beta && patch -p1 < "$(buildprefix)/Patches/neutrino.beta.diff"
-	cd $(appsdir)/neutrino-beta && patch -p1 < "$(buildprefix)/Patches/neutrino.libcool.beta.diff"
+BEGIN[[
+neutrino_hd
+  git
+  {PN}-nightly
+
+ifdef ENABLE_NHD0
+  git://gitorious.org/neutrino-hd/neutrino-hd.git
+endif
+
+ifdef ENABLE_NHD1
+  #svn://neutrinohd2.googlecode.com/svn/trunk/neutrino-hd
+endif
+
+ifdef ENABLE_NHD2
+  git://gitorious.org/neutrino-mp/neutrino-mp.git
+endif
+;
+]]END
+
+
+N_CPPFLAGS = -I$(driverdir)/bpamem
+
+ifdef ENABLE_SPARK
+N_CPPFLAGS += -I$(driverdir)/frontcontroller/aotom
+endif
+
+ifdef ENABLE_SPARK7162
+N_CPPFLAGS += -I$(driverdir)/frontcontroller/aotom
+endif
+
+N_CONFIG_OPTS = --enable-silent-rules --enable-freesatepg
+
+ifdef ENABLE_EXTERNALLCD
+N_CONFIG_OPTS += --enable-graphlcd
+endif
+
+$(DEPDIR)/neutrino-hd-nightly.do_prepare: $(DEPENDS_neutrino_hd)
+	$(PREPARE_neutrino_hd)
 	touch $@
 
-$(appsdir)/neutrino-beta/config.status: bootstrap $(EXTERNALLCD_DEP) freetype libjpeg libpng libgif libid3tag curl libmad libvorbisidec libboost openssl libopenthreads sdparm
+$(DIR_neutrino_hd)/config.status: bootstrap freetype $(EXTERNALLCD_DEP) libjpeg libpng libgif libid3tag curl libmad libvorbisidec libboost libflac openssl sdparm 
 	export PATH=$(hostprefix)/bin:$(PATH) && \
-	cd $(appsdir)/neutrino-beta && \
+	cd $(DIR_neutrino_hd) && \
 		ACLOCAL_FLAGS="-I $(hostprefix)/share/aclocal" ./autogen.sh && \
 		$(BUILDENV) \
 		./configure \
 			--host=$(target) \
 			$(N_CONFIG_OPTS) \
+			--enable-libeplayer3 \
+			--with-boxtype=none \
+			--enable-pcmsoftdecoder \
 			--with-tremor \
-			--with-libdir=/usr/lib \
-			--with-datadir=/share/tuxbox \
-			--with-fontdir=/share/fonts \
-			--with-configdir=/var/tuxbox/config \
-			--with-gamesdir=/var/tuxbox/games \
-			--with-plugindir=/usr/lib/tuxbox/plugins \
-			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
-			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
-			$(PLATFORM_CPPFLAGS) \
-			CPPFLAGS="$(N_CPPFLAGS)"
-
-
-$(DEPDIR)/neutrino-beta.do_compile: $(appsdir)/neutrino-beta/config.status
-	cd $(appsdir)/neutrino-beta && \
-		$(MAKE) all
-	touch $@
-
-$(DEPDIR)/neutrino-beta: neutrino-beta.do_prepare neutrino-beta.do_compile
-	$(MAKE) -C $(appsdir)/neutrino-beta install DESTDIR=$(targetprefix) && \
-	make $(targetprefix)/var/etc/.version
-	$(target)-strip $(targetprefix)/usr/local/bin/neutrino
-	$(target)-strip $(targetprefix)/usr/local/bin/pzapit
-	$(target)-strip $(targetprefix)/usr/local/bin/sectionsdcontrol
-	touch $@
-
-neutrino-beta-clean:
-	rm -f $(DEPDIR)/neutrino-beta
-	cd $(appsdir)/neutrino-beta && \
-		$(MAKE) distclean
-
-neutrino-beta-distclean:
-	rm -f $(DEPDIR)/neutrino-beta
-	rm -f $(DEPDIR)/neutrino-beta.do_compile
-	rm -f $(DEPDIR)/neutrino-beta.do_prepare
-	rm -rf $(appsdir)/neutrino-nightly.org
-	rm -rf $(appsdir)/neutrino-nightly
-
-#
-#NORMAL
-#
-$(appsdir)/neutrino/config.status: bootstrap $(EXTERNALLCD_DEP) freetype libpng libid3tag openssl curl libmad libboost libgif sdparm
-	export PATH=$(hostprefix)/bin:$(PATH) && \
-	cd $(appsdir)/neutrino && \
-		ACLOCAL_FLAGS="-I $(hostprefix)/share/aclocal" ./autogen.sh && \
-		$(BUILDENV) \
-		./configure \
-			--host=$(target) \
-			--without-libsdl \
+			--enable-libass \
 			--with-datadir=/usr/local/share \
 			--with-libdir=/usr/lib \
 			--with-plugindir=/usr/lib/tuxbox/plugins \
@@ -91,26 +63,30 @@ $(appsdir)/neutrino/config.status: bootstrap $(EXTERNALLCD_DEP) freetype libpng 
 			--with-gamesdir=/usr/local/share/games \
 			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
 			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
-			$(PLATFORM_CPPFLAGS)
+			$(PLATFORM_CPPFLAGS) \
+			CPPFLAGS="$(N_CPPFLAGS)"
 
-$(DEPDIR)/neutrino.do_prepare: Patches/neutrino.patch
-	touch $@
 
-$(DEPDIR)/neutrino.do_compile: $(appsdir)/neutrino/config.status
-	cd $(appsdir)/neutrino && \
+$(DEPDIR)/neutrino-hd-nightly.do_compile: $(DIR_neutrino_hd)/config.status
+	cd $(DIR_neutrino_hd) && \
 		$(MAKE) all
 	touch $@
 
-$(DEPDIR)/neutrino: neutrino.do_prepare neutrino.do_compile
-	$(MAKE) -C $(appsdir)/neutrino install DESTDIR=$(targetprefix) DATADIR=$(targetprefix)/usr/local/share/
-	$(target)-strip $(targetprefix)/usr/local/bin/neutrino
-	$(target)-strip $(targetprefix)/usr/local/bin/pzapit
-	$(target)-strip $(targetprefix)/usr/local/bin/sectionsdcontrol
+$(DEPDIR)/neutrino-hd-nightly: neutrino-hd-nightly.do_prepare neutrino-hd-nightly.do_compile
+	$(MAKE) -C $(DIR_neutrino_hd) install DESTDIR=$(PKDIR) && \
+	make $(PKDIR)/var/etc/.version
+	$(target)-strip $(PKDIR)/usr/local/bin/neutrino
+	$(target)-strip $(PKDIR)/usr/local/bin/pzapit
+	$(target)-strip $(PKDIR)/usr/local/bin/sectionsdcontrol
 	touch $@
 
-neutrino-clean neutrino-distclean:
-	rm -f $(DEPDIR)/neutrino
-	rm -f $(DEPDIR)/neutrino.do_compile
-	rm -f $(DEPDIR)/neutrino.do_prepare
-	cd $(appsdir)/neutrino && \
+neutrino-hd-nightly-clean:
+	rm -f $(DEPDIR)/neutrino-hd-nightly
+	cd $(DIR_neutrino_hd) && \
 		$(MAKE) distclean
+
+neutrino-hd-nightly-distclean:
+	rm -f $(DEPDIR)/neutrino-hd-nightly
+	rm -f $(DEPDIR)/neutrino-hd-nightly.do_compile
+	rm -f $(DEPDIR)/neutrino-hd-nightly.do_prepare
+	rm -rf $(DIR_neutrino_hd)
