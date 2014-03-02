5c8bd984712b0106d123ce6fe091a07d45544dd9
schpuntik <schpuntik@freenet.de>
2014-03-02 15:02:03 +0100

    gstreamer change  to  separate file

--- make/root-etc.mk
+++
@@ -1,8 +0,0 @@
-define initdconfig
-	export HHL_CROSS_TARGET_DIR=$(prefix)/release && \
-	cd $(prefix)/release/etc/init.d && \
-		for s in $(1) ; do \
-			$(hostprefix)/bin/target-initdconfig --add $$s || echo "Unable to enable initd service: $$s" ; \
-		done && \
-		rm *rpmsave 2>/dev/null || true
-endef
