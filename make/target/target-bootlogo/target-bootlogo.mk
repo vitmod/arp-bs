91b9d06b491878fb52f39efd5250ee9da9807869
schpuntik <schpuntik@freenet.de>
2014-05-01 21:09:43 +0200

    add package bootlogo

---
+++ make/target/target-bootlogo/target-bootlogo.mk
@@ -0,0 +1,35 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_bootlogo
+
+BDEPENDS_${P} = $(target_filesystem)
+
+PV_${P} = 0.1
+PR_${P} = 1
+
+DESCRIPTION_${P} = bootlogo.mvi
+
+call[[ base ]]
+
+rule[[
+  pdircreate:${PN}-${PV}
+  install:-d:$(PKDIR)/boot
+  install_file:$(PKDIR)/boot/bootlogo.mvi:file://../root/bootscreen/bootlogo.mvi
+]]rule
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_prepare
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && $(INSTALL_${P})
+	touch $@
+
+NAME_${P} = bootlogo
+SRC_URI_${P} = unknown
+
+call[[ ipkbox ]]
+
+]]package
