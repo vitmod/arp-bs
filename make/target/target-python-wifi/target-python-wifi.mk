c327d3496a2a3b36057bd35ce47e6d9ffc65e86e
schpuntik <schpuntik@freenet.de>
2014-05-07 22:49:23 +0200

    add python-wifi

---
+++ make/target/target-python-wifi/target-python-wifi.mk
@@ -0,0 +1,40 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_python_wifi
+
+BDEPENDS_${P} = $(target_python_setuptools)
+
+PV_${P} = 0.5.0
+PR_${P} = 1
+
+call[[ base ]]
+
+rule[[
+  extract:http://freefr.dl.sourceforge.net/project/pythonwifi.berlios/python-wifi-${PV}.tar.bz2
+]]rule
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && $(python_build)
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && $(python_install)
+	touch $@
+
+call[[ ipk ]]
+
+
+DESCRIPTION_${P} = Python WiFi is a Python module that provides read and write access \
+	to a wireless network card's capabilities using the Linux Wireless Extensions
+RDEPENDS_${P} = python_core python_ctypes python_datetime
+FILES_${P} = $(PYTHON_DIR)/site-packages/pythonwifi
+
+call[[ ipkbox ]]
+
+]]package
