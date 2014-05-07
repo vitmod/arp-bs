8cb28d6398f80ea8db807cdb14aefa33e40c973f
schpuntik <schpuntik@freenet.de>
2014-05-07 22:49:55 +0200

    add rfkill

---
+++ make/target/target-rfkill/target-rfkill.mk
@@ -0,0 +1,39 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_rfkill
+
+BDEPENDS_${P} = $(target_glibc)
+
+PV_${P} = git
+PR_${P} = 1
+
+call[[ base ]]
+
+rule[[
+  nothing:git://git.sipsolutions.net/rfkill.git
+]]rule
+
+call[[ git ]]
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && \
+		$(MAKE) $(MAKE_OPTS)
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && $(MAKE) install DESTDIR=$(PKDIR)
+	touch $@
+
+DESCRIPTION_${P} = rfkill is a small tool to query the state of the rfkill switches, buttons and subsystem interfaces
+RDEPENDS_${P} = libc6
+FILES_${P} = /usr/sbin/*
+
+call[[ ipkbox ]]
+
+]]package
