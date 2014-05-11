83b65ce56ca58616656b25d281903996e38e70e9
schpuntik <schpuntik@freenet.de>
2014-05-11 13:04:53 +0200

    evremote2   add depends

--- make/target/target-tdt-tools/target-evremote2.mk
+++ make/target/target-tdt-tools/target-evremote2.mk
@@ -3,11 +3,12 @@
 #
 package[[ target_evremote2
 
-BDEPENDS_${P} = $(target_glibc)
+BDEPENDS_${P} = $(target_glibc) $(target_lirc) $(target_initscripts)
 
 PR_${P} = $(PR_tdt_tools).1
 
 DESCRIPTION_${P} = evremote2
+RDEPENDS_${P} = lirc initscripts
 FILES_${P} = /bin/evremote2 /bin/evtest
 
 
