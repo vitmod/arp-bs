b223c6536ccef0c4f4111e5a7a341e8776e6c416
schpuntik <schpuntik@freenet.de>
2014-05-22 00:46:32 +0200

    add libogg package

---
+++ make/target/target-libogg/target-libogg.mk
@@ -0,0 +1,46 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_libogg
+
+BDEPENDS_${P} = $(target_glibc)
+
+PV_${P} = 1.3.1
+PR_${P} = 1
+
+call[[ base ]]
+
+rule[[
+  extract:http://downloads.xiph.org/releases/ogg/${PN}-${PV}.tar.xz
+]]rule
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && \
+		$(BUILDENV) \
+		./configure \
+			--build=$(build) \
+			--host=$(target) \
+			--prefix=/usr \
+		&& \
+		make
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && $(MAKE) install DESTDIR=$(PKDIR)
+	touch $@
+
+call[[ ipk ]]
+
+NAME_${P} = libogg0
+DESCRIPTION_${P} = libogg is the bitstream and framing library for the Ogg project. \
+It provides functions which are necessary to codec libraries like libvorbis.
+FILES_${P} = /usr/lib/*.so*
+
+call[[ ipkbox ]]
+
+]]package
