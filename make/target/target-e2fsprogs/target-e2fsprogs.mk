9a914288e2176e7ae59a8ee480cca417c271e1d5
schpuntik <schpuntik@freenet.de>
2014-05-31 23:34:57 +0200

    optimize  DESCRIPTION_e2fsprogs

--- make/target/target-e2fsprogs/target-e2fsprogs.mk
+++ make/target/target-e2fsprogs/target-e2fsprogs.mk
@@ -64,31 +64,23 @@ PACKAGES_${P} = e2fsprogs_badblocks \
 		libext2fs2 \
 		libss2
 
-DESCRIPTION_e2fsprogs_badblocks = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
+DESCRIPTION_${P} = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
  utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_2fsprogs_badblocks = libcom-err2 libext2fs2 libc6
 FILES_e2fsprogs_badblocks = /sbin/badblocks
 
-DESCRIPTION_e2fsprogs_e2fsck = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_e2fsprogs_e2fsck = libblkid1 libcom_err2 libe2p2 libc6 libext2fs2 libuuid1
 FILES_e2fsprogs_e2fsck = /sbin/e2fsck
 
-DESCRIPTION_e2fsprogs_mke2fs = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_e2fsprogs_mke2fs = libblkid1 libcom_err2 libe2p2 libc6 libext2fs2 libuuid1
 FILES_e2fsprogs_mke2fs = /etc/mke2fs.conf \
 			/sbin/mkfs* \
 			/sbin/mke2fs
 
-DESCRIPTION_e2fsprogs_tune2fs = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_e2fsprogs_tune2fs = libblkid1 libcom_err2 libe2p2 libc6 libext2fs2 libuuid1
 FILES_e2fsprogs_tune2fs = /sbin/tune2fs \
 			  /sbin/e2label
 
-DESCRIPTION_e2fsprogs_core = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_e2fsprogs_core = libss2 libext2fs2 libcom_err2 libe2p2 libc6 e2fsprogs_badblocks libblkid1 libuuid1
 FILES_e2fsprogs_core = /sbin/debugfs \
 		  /sbin/dumpe2fs \
@@ -101,22 +93,14 @@ FILES_e2fsprogs_core = /sbin/debugfs \
 		  /usr/bin/lsattr \
 		  /usr/bin/mk_cmds
 
-DESCRIPTION_libcom_err2 = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 FILES_libcom_err2 = /lib/libcom_err.so.*
 
-DESCRIPTION_libe2p2 = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 FILES_libe2p2 = /lib/libe2p.so.*
 
-DESCRIPTION_libext2fs2 = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_libext2fs2 = libblkid1 libcom_err2
 FILES_libext2fs2 = /lib/libext2fs.so.* \
 		   /usr/lib/e2initrd_helper
 
-DESCRIPTION_libss2 = The Ext2 Filesystem Utilities (e2fsprogs) contain all of the standard \
- utilities for creating, fixing, configuring , and debugging ext2 filesystems.
 RDEPENDS_libss2 = libcom_err2
 FILES_libss2 = /lib/libss.so.*
 
