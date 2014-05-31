03cb785bdd4bae6839934d24ef99f2190e999f81
schpuntik <schpuntik@freenet.de>
2014-06-01 00:01:50 +0200

    optimize  DESCRIPTION util-linux

--- make/target/target-util-linux/target-util-linux.mk
+++ make/target/target-util-linux/target-util-linux.mk
@@ -27,53 +27,25 @@ PACKAGES_${P} = util_linux_agetty \
 		libuuid1
  
 
-DESCRIPTION_util_linux_agetty = Util-linux includes a suite of basic system administration utilities \
+DESCRIPTION_${P} = Util-linux includes a suite of basic system administration utilities \
  commonly found on most Linux systems.  Some of the more important \
  utilities include disk partitioning, kernel message management, \
  filesystem creation, and system login.
 FILES_util_linux_agetty = /sbin/agetty
 
-DESCRIPTION_util_linux_sfdisk = A suite of basic system administration utilities. \
- Util-linux includes a suite of basic system administration utilities \
- commonly found on most Linux systems.  Some of the more important \
- utilities include disk partitioning, kernel message management, \
- filesystem creation, and system login.
 FILES_util_linux_sfdisk = /sbin/sfdisk
 
-DESCRIPTION_util_linux_blkid =  Util-linux includes a suite of basic system administration utilities \
- commonly found on most Linux systems.  Some of the more important \
- utilities include disk partitioning, kernel message management, \
- filesystem creation, and system login.
+RDEPENDS_util_linux_blkid = libblkid1
 FILES_util_linux_blkid = /sbin/blkid
 
-DESCRIPTION_util_linux_cfdisk = A suite of basic system administration utilities. \
- Util-linux includes a suite of basic system administration utilities \
- commonly found on most Linux systems.  Some of the more important \
- utilities include disk partitioning, kernel message management, \
- filesystem creation, and system login.
 RDEPENDS_util_linux_cfdisk = libblkid1 libncurses5
 FILES_util_linux_cfdisk = /sbin/cfdisk
 
-DESCRIPTION_util_linux_fdisk = A suite of basic system administration utilities. \
- Util-linux includes a suite of basic system administration utilities \
- commonly found on most Linux systems.  Some of the more important \
- utilities include disk partitioning, kernel message management, \
- filesystem creation, and system login.
 FILES_util_linux_fdisk = /sbin/fdisk
 
-DESCRIPTION_libblkid1 = A suite of basic system administration utilities. \
- Util-linux includes a suite of basic system administration utilities \
- commonly found on most Linux systems.  Some of the more important \
- utilities include disk partitioning, kernel message management, \
- filesystem creation, and system login.
 RDEPENDS_libblkid1 = libuuid1
 FILES_libblkid1 = /usr/lib/libblkid.so.*
 
-DESCRIPTION_libuuid1 = A suite of basic system administration utilities. \
- Util-linux includes a suite of basic system administration utilities \
- commonly found on most Linux systems.  Some of the more important \
- utilities include disk partitioning, kernel message management, \
- filesystem creation, and system login.
 FILES_libuuid1 = /usr/lib/libuuid.so.*
 
 call[[ ipkbox ]]
