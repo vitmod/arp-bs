135eb1328e335f233a8ce4b67a3b6721d487a62a
schpuntik <schpuntik@freenet.de>
2014-07-02 20:33:49 +0200

    update glibc

--- make/target/target-glibc/target-glibc.mk
+++ make/target/target-glibc/target-glibc.mk
@@ -8,7 +8,7 @@ BREPLACES_${P} = $(target_glibc_first) $(target_glibc_headers)
 
 PR_${P} = 1
 
-${P}_VERSION := 2.14.1-48
+${P}_VERSION := 2.14.1-50
 ${P}_SPEC = stm-$(${P}).spec
 ${P}_SPEC_PATCH = 
 ${P}_PATCHES = 
