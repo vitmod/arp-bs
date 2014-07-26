63e310e67ecfb50ec20b1bbeb5ec296a617b6e2b
schpuntik <schpuntik@freenet.de>
2014-07-26 18:04:44 +0200

    fix  cross-kernel-headers

--- make/cross/cross-linux-kernel-headers/cross-kernel-headers.mk
+++ make/cross/cross-linux-kernel-headers/cross-kernel-headers.mk
@@ -8,8 +8,12 @@ package[[ target_kernel_headers
 BDEPENDS_${P} = $(target_filesystem)
 
 PR_${P} = 2
-
+ifeq ($(CONFIG_KERNEL_0211),y)
 ${P}_VERSION = 2.6.32.46-47
+endif
+ifeq ($(CONFIG_KERNEL_0215),y)
+${P}_VERSION = 2.6.32.46-48
+endif
 ${P}_SPEC = stm-target-${PN}-kbuild.spec
 ${P}_SPEC_PATCH =
 ${P}_PATCHES =
