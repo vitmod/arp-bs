e7af2e95457886a6ca577ee1a6452995d226f54f
schpuntik <schpuntik@freenet.de>
2014-08-13 12:02:22 +0200

    usb-modeswitch update to 2.2.0

--- make/target/target-usb-modeswitch/target-usb-modeswitch.mk
+++ make/target/target-usb-modeswitch/target-usb-modeswitch.mk
@@ -5,7 +5,7 @@ package[[ target_usb_modeswitch
 
 BDEPENDS_${P} = $(target_libusb) $(target_libusb_compat) $(target_usb_modeswitch_data)
 
-PV_${P} = 2.1.1
+PV_${P} = 2.2.0
 PR_${P} = 1
 
 call[[ base ]]
