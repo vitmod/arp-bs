d20a85f973d01efcae18bdf525b36b3e278707d9
Alex <alexeytech@gmail.com>
2014-08-20 21:30:06 +0200

    base-rpm set buildroot=$(PKDIR), add prepare
    also rename functions.

--- make/base-rpm.mk
+++ make/base-rpm.mk
@@ -1,5 +1,4 @@
-fromrpm_copy = cp -ar $(prefix)/BUILDROOT/$(subst src.rpm^,*,$(notdir $(${P}_SRCRPM))^)/* $(PKDIR)
-
+# for building rpms from stlinux
 function[[ base_rpm
 
 # place after variables definitions in *.mk file and before TARGET
@@ -11,24 +10,30 @@ DEPENDS_${P} += $(${P}_SRCRPM)
 
 ]]function
 
-function[[ TARGET_rpm_do_compile
+function[[ rpm_do_prepare
 
-$(TARGET_${P}).do_compile: $(DEPENDS_${P})
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
 	$(rpm_src_install) $(${P}_SRCRPM)
 	$(if $(${P}_SPEC_PATCH), cd $(specsprefix) && patch -p1 $(${P}_SPEC) < ${SDIR}/$(${P}_SPEC_PATCH) )
 	$(if $(${P}_PATCHES), cp $(addprefix ${SDIR}/,$(${P}_PATCHES)) $(sourcesprefix) )
-#	rm -rf $(prefix)/BUILDROOT/*
+	touch $@
+
+]]function
+
+#FIXME:
+function[[ rpm_do_compile
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	$(PKDIR_clean)
 	$(rpm_build) $(specsprefix)/$(${P}_SPEC)
 	touch $@
 ]]function
 
-function[[ TARGET_rpm_do_package
+function[[ rpm_do_package
 
 DO_PACKAGE_${P} ?=
 
 $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
-	$(PKDIR_clean)
-	$(fromrpm_copy)
 ifeq (${SYSROOT},target)
 	mv $(PKDIR)/$(targetprefix)/* $(PKDIR)
 endif
@@ -37,32 +42,28 @@ endif
 
 ]]function
 
-function[[ TARGET_host_rpm
-# place after all variables definitions in *.mk file
-call[[ TARGET_rpm_do_compile ]]
-call[[ TARGET_rpm_do_package ]]
+function[[ rpm
+call[[ rpm_do_prepare ]]
+call[[ rpm_do_compile ]]
+call[[ rpm_do_package ]]
+]]function
 
+# FIXME: rename
+function[[ TARGET_host_rpm
+call[[ rpm ]]
 ]]function
 
 function[[ TARGET_cross_rpm
-# place after all variables definitions in *.mk file
-call[[ TARGET_rpm_do_compile ]]
-call[[ TARGET_rpm_do_package ]]
-
+call[[ rpm ]]
 ]]function
 
 function[[ TARGET_target_rpm
-# place after all variables definitions in *.mk file
-call[[ TARGET_rpm_do_compile ]]
-call[[ TARGET_rpm_do_package ]]
-
+call[[ rpm ]]
 ]]function
 
-function[[ rpm_do_compile
-call[[TARGET_rpm_do_compile]]
+function[[ TARGET_rpm_do_compile
+call[[ rpm_do_compile ]]
 ]]function
-
-function[[ rpm
-call[[ TARGET_rpm_do_compile ]]
-call[[ TARGET_rpm_do_package ]]
+function[[ TARGET_rpm_do_package
+call[[ rpm_do_package ]]
 ]]function
