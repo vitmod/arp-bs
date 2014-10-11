77a8ba6b27972914bfa699e474f67f22c082d915
schpuntik <schpuntik@freenet.de>
2014-10-11 21:17:25 +0200

    add depends libass to libeplayer3

--- make/target/target-tdt-tools/target-libeplayer3.mk
+++ make/target/target-tdt-tools/target-libeplayer3.mk
@@ -8,7 +8,7 @@ BDEPENDS_${P} = $(target_glibc) $(target_gcc_lib) $(target_driver) $(target_liba
 PR_${P} = $(PR_tdt_tools).1
 
 DESCRIPTION_${P} = libeplayer3
-RDEPENDS_${P} = kernel_module_player2 kernel_module_stgfb ffmpeg
+RDEPENDS_${P} = kernel_module_player2 kernel_module_stgfb ffmpeg libass
 FILES_${P} = \
 	/bin/eplayer3 \
 	/bin/meta \
