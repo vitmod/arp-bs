52d2b6af80af4bae2379e98c01e13c9ceb0c9e12
schpuntik <schpuntik@freenet.de>
2014-10-11 21:11:41 +0200

    add skin metropolishd

---
+++ make/target/target-enigma2-skins/target-skins-metropolishd.mk
@@ -0,0 +1,38 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_enigma2_skins_metropolishd
+
+BDEPENDS_${P} = $(target_filesystem)
+
+PV_${P} = git
+PR_${P} = 1
+
+call[[ base ]]
+
+rule[[
+  git://github.com/Taapat/skin-MetropolisHD
+]]rule
+
+call[[ git ]]
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_prepare
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && cp -dpR $(DIR_${P})/usr  $(PKDIR)
+	touch $@
+
+call[[ ipk ]]
+
+NAME_${P} = enigma2-plugin-skin-metropolishd
+DESCRIPTION_${P} = Skin MetropolisHD for enigma2
+RDEPENDS_${P} = enigma2
+FILES_${P} = /usr
+
+call[[ ipkbox ]]
+
+]]package
