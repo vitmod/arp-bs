cf7886e37d493f6003389a6719b4b327142c4bbf
schpuntik <schpuntik@freenet.de>
2014-10-11 21:13:18 +0200

    remove  build skins by enigma2

--- make/target/target-enigma2/target-enimga2.mk
+++ make/target/target-enigma2/target-enimga2.mk
@@ -99,7 +99,7 @@ endif
 ifdef CONFIG_EXTERNAL_LCD
 BDEPENDS_${P} += $(target_graphlcd)
 CONFIG_FLAGS_${P} += --with-graphlcd
-RDEPENDS_enigma2 = libgraphlcd
+RDEPENDS_enigma2 += libgraphlcd
 endif
 
 call[[ base ]]
@@ -196,9 +196,7 @@ PACKAGES_${P} = \
 	enigma2_plugin_systemplugins_videoenhancement \
 	enigma2_plugin_systemplugins_videotune \
 	enigma2_plugin_systemplugins_videomode \
-	enigma2_plugin_systemplugins_wirelesslan \
-	enigma2_plugin_skin_magic \
-	enigma2_plugin_skin_megamod
+	enigma2_plugin_systemplugins_wirelesslan
 
 RDEPENDS_enigma2 += libgcc1 libpython2.7 python-threading libtuxtxt0 libbz2 libgif4 libfreetype6 python-core python-twisted-core libdvbsi++1 python-re font-ae-almateen font-andale font-lcd font-md-khmurabi font-tuxtxt font-nmsbd libpng16 font-valis-enigma libstdc++6 libglib libsigc-1.2 python-fcntl python-netclient python-netserver python-codecs libcrypto1 libfribidi0 python-zopeinterface python-xml libtuxtxt32bpp0 python-pickle libxmlccwrap python-shell ethtool libjpeg8 libdreamdvd0 python-twisted-web python-zlib python-crypt python-lang python-subprocess enigma2_meta python-compression
 FILES_enigma2 = \
@@ -351,14 +349,6 @@ FILES_enigma2_plugin_systemplugins_videomode = /usr/lib/enigma2/python/Plugins/S
 DESCRIPTION_enigma2_plugin_systemplugins_wirelesslan = The wireless lan plugin helps you configuring your WLAN network interface.
 RDEPENDS_enigma2_plugin_systemplugins_wirelesslan = enigma2 python_wifi
 FILES_enigma2_plugin_systemplugins_wirelesslan = /usr/lib/enigma2/python/Plugins/SystemPlugins/WirelessLan
-
-DESCRIPTION_enigma2_plugin_skin_magic = Skin Magic for SD video
-RDEPENDS_enigma2_plugin_skin_magic = enigma2
-FILES_enigma2_plugin_skin_magic = /usr/share/enigma2/Magic
-
-DESCRIPTION_enigma2_plugin_skin_megamod = Skin megaMod  for HD video
-RDEPENDS_enigma2_plugin_skin_megamod = enigma2 enigma2_plugin_systemplugins_libgisclubskin
-FILES_enigma2_plugin_skin_megamod = /usr/share/enigma2/megaMod
 
 call[[ ipkbox ]]
 
