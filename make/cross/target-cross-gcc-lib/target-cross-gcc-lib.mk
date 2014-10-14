c29943ecadbe89890212efc64e028c77f729f6a0
Alex <alexeytech@gmail.com>
2014-10-14 22:39:52 +0200

    target-gcc-lib reduce depends for rm_work
    actually we need WORK_cross_gcc_second until we have copied files in do_package

--- make/cross/target-cross-gcc-lib/target-cross-gcc-lib.mk
+++ make/cross/target-cross-gcc-lib/target-cross-gcc-lib.mk
@@ -1,6 +1,10 @@
 #
 # LIBS FROM GCC
 #
+
+# This is a solution for the case, when we have packages for cross and target
+# from the same build dir
+# There is NO reason in splitting toolchain ipk's when they goes to same SYSROOT.
 package[[ target_cross_gcc_lib
 
 BDEPENDS_${P} = $(cross_gcc_second)
@@ -18,15 +22,14 @@ $(TARGET_${P}).do_package: $(cross_gcc_second).do_package
 	cp -ar $(WORK_cross_gcc_second)/root/$(targetprefix)/* $(PKDIR)
 	touch $@
 
-call[[ ipk ]]
 # Ask to not RM_WORK util we finished
-$(cross_gcc_second): $(TARGET_${P}).do_ipk
+$(cross_gcc_second): $(TARGET_${P}).do_package
+
+call[[ ipk ]]
 
 ifdef CONFIG_GCC_LIB_FROM_CROSS
 call[[ target_gcc_lib_in ]]
 call[[ ipkbox ]]
-# Ask to not RM_WORK util we finished
-$(cross_gcc_second): $(TARGET_${P}).do_ipkbox
 endif
 
 ]]package
