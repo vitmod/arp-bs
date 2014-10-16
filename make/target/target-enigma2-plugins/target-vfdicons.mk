b73718e1d10bbf92a12d426040ead193dfe12ec9
schpuntik <schpuntik@freenet.de>
2014-10-17 00:02:18 +0200

    add build vfdicons  plugin

---
+++ make/target/target-enigma2-plugins/target-vfdicons.mk
@@ -0,0 +1,38 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_vfdicons
+
+BDEPENDS_${P} = $(target_python)
+
+PV_${P} = git
+PR_${P} = 2
+
+call[[ base ]]
+
+rule[[
+  nothing:git://github.com/OpenAR-P/VFD-Icons.git:b=master
+]]rule
+
+call[[ git ]]
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_prepare
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && \
+		mkdir -p $(PKDIR)/usr/lib/enigma2/python/Plugins/SystemPlugins && \
+		cp -a plugin $(PKDIR)/usr/lib/enigma2/python/Plugins/SystemPlugins/VFD-Icons && \
+	touch $@
+
+NAME_${P} = enigma2-plugin-systemplugins-vfd-icons
+DESCRIPTION_${P} = "open VFD-icons plugin for enigma2 by openAR-P teamv full simbols for Alien²"
+RDEPENDS_${P} = python_core
+RCONFLICTS_${P} = enigma2-plugin-systemplugins-minivfd-icons
+FILES_${P} = /usr/lib/enigma2/python/Plugins/SystemPlugins/VFD-Icons
+
+call[[ ipkbox ]]
+
+]]package
