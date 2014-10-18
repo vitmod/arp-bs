6f21b4fd2f2d8f861d1bf9631fc7e8b8155415f8
schpuntik <schpuntik@freenet.de>
2014-10-18 21:46:01 +0200

    add eplayer4 build thx  technik

---
+++ make/target/target-tdt-tools/target-eplayer4.mk
@@ -0,0 +1,18 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_eplayer4
+
+BDEPENDS_${P} = $(target_glibc) $(target_gstreamer)
+
+PR_${P} = $(PR_tdt_tools).1
+
+DESCRIPTION_${P} = eplayer4
+FILES_${P} = /bin/eplayer4
+
+
+call[[ base_tdt_tools ]]
+
+call[[ TARGET_tdt_tools ]]
+
+]]package
