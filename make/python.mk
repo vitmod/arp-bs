88dddf70cea3d7d64e02805263e6efced8613a55
schpuntik <schpuntik@freenet.de>
2014-10-19 23:36:34 +0200

    clean git

--- make/python.mk
+++ make/python.mk
@@ -34,43 +34,6 @@ $(DEPDIR)/pylimaging: bootstrap python setuptools $(DEPENDS_pylimaging)
 
 
 
-#
-# pyusb
-#
-BEGIN[[
-pyusb
-  1.0.0a3
-  {PN}-{PV}
-  extract:http://pypi.python.org/packages/source/p/{PN}/{PN}-{PV}.tar.gz
-;
-]]END
-NAME_pyusb = python_pyusb
-DESCRIPTION_pyusb =  PyUSB provides USB access on the Python language
-RDEPENDS_pyusb = python_core
-FILES_pyusb = \
-$(PYTHON_DIR)/site-packages/usb/*
-
-$(DEPDIR)/pyusb.do_prepare: bootstrap setuptools $(DEPENDS_pyusb)
-	$(PREPARE_pyusb)
-	touch $@
-
-$(DEPDIR)/pyusb.do_compile: $(DEPDIR)/pyusb.do_prepare
-	cd $(DIR_pyusb) && \
-		CC='$(target)-gcc' LDSHARED='$(target)-gcc -shared' \
-		PYTHONPATH=$(targetprefix)$(PYTHON_DIR)/site-packages \
-		$(crossprefix)/bin/python ./setup.py build
-	touch $@
-
-$(DEPDIR)/pyusb: $(DEPDIR)/pyusb.do_compile
-	$(start_build)
-	cd $(DIR_pyusb) && \
-		PYTHONPATH=$(targetprefix)$(PYTHON_DIR)/site-packages \
-		$(crossprefix)/bin/python ./setup.py install --root=$(PKDIR) --prefix=/usr
-	$(tocdk_build)
-	$(toflash_build)
-	touch $@
-
-
 
 
 
@@ -105,49 +68,6 @@ $(DEPDIR)/mutagen.do_compile: $(DEPDIR)/mutagen.do_prepare
 $(DEPDIR)/mutagen: $(DEPDIR)/mutagen.do_compile
 	$(start_build)
 	cd $(DIR_mutagen) && \
-		CC='$(target)-gcc' LDSHARED='$(target)-gcc -shared' \
-		PYTHONPATH=$(targetprefix)$(PYTHON_DIR)/site-packages \
-		$(crossprefix)/bin/python ./setup.py install --root=$(PKDIR) --prefix=/usr
-	$(tocdk_build)
-	$(remove_pyc)
-	$(toflash_build)
-	touch $@
-
-
-#
-# gdata
-#
-BEGIN[[
-gdata
-  2.0.18
-  gdata-{PV}
-  extract:http://gdata-python-client.googlecode.com/files/gdata-{PV}.tar.gz
-;
-]]END
-
-NAME_gdata = python_gdata
-DESCRIPTION_gdata = Google Data APIs Python Client Library
-RDEPENDS_gdata = python_core python_elementtree
-FILES_gdata = \
-  $(PYTHON_DIR)/site-packages/atom/*.* \
-  $(PYTHON_DIR)/site-packages/gdata/*.* \
-  $(PYTHON_DIR)/site-packages/gdata/
-
-
-$(DEPDIR)/gdata.do_prepare: bootstrap setuptools $(DEPENDS_gdata)
-	$(PREPARE_gdata)
-	touch $@
-
-$(DEPDIR)/gdata.do_compile: $(DEPDIR)/gdata.do_prepare
-	cd $(DIR_gdata) && \
-		CC='$(target)-gcc' LDSHARED='$(target)-gcc -shared' \
-		PYTHONPATH=$(targetprefix)$(PYTHON_DIR)/site-packages \
-		$(crossprefix)/bin/python -c "import setuptools; execfile('setup.py')" build
-	touch $@
-
-$(DEPDIR)/gdata: $(DEPDIR)/gdata.do_compile
-	$(start_build)
-	cd $(DIR_gdata) && \
 		CC='$(target)-gcc' LDSHARED='$(target)-gcc -shared' \
 		PYTHONPATH=$(targetprefix)$(PYTHON_DIR)/site-packages \
 		$(crossprefix)/bin/python ./setup.py install --root=$(PKDIR) --prefix=/usr
@@ -196,38 +116,4 @@ $(DEPDIR)/lxml: $(DEPDIR)/lxml.do_compile
 	$(tocdk_build)
 	$(remove_pyc)
 	$(extra_build)
-	touch $@
-
-
-
-#
-# elementtree
-#
-BEGIN[[
-elementtree
-  1.2.6-20050316
-  {PN}-{PV}
-  extract:http://effbot.org/media/downloads/{PN}-{PV}.tar.gz
-;
-]]END
-
-DESCRIPTION_elementtree = "Provides light-weight components for working with XML"
-FILES_elementtree = \
-$(PYTHON_DIR)
-
-$(DEPDIR)/elementtree.do_prepare: bootstrap $(DEPENDS_elementtree)
-	$(PREPARE_elementtree)
-	touch $@
-
-$(DEPDIR)/elementtree.do_compile: $(DEPDIR)/elementtree.do_prepare
-	touch $@
-
-$(DEPDIR)/elementtree: $(DEPDIR)/elementtree.do_compile
-	$(start_build)
-	cd $(DIR_elementtree) && \
-		CC='$(target)-gcc' LDSHARED='$(target)-gcc -shared' \
-		$(crossprefix)/bin/python ./setup.py install --root=$(PKDIR) --prefix=/usr
-	$(tocdk_build)
-	$(remove_pyc)
-	$(toflash_build)
 	touch $@
