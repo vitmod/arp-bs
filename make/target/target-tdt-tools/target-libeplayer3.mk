ba7f9d5e88afc547c240868ce194f46181242784
schpuntik <schpuntik@freenet.de>
2014-11-12 20:25:27 +0100

    target-libeplayer3  add prefix /usr

--- make/target/target-tdt-tools/target-libeplayer3.mk
+++ make/target/target-tdt-tools/target-libeplayer3.mk
@@ -10,9 +10,9 @@ PR_${P} = $(PR_tdt_tools).1
 DESCRIPTION_${P} = libeplayer3
 RDEPENDS_${P} = kernel_module_player2 kernel_module_stgfb ffmpeg libass
 FILES_${P} = \
-	/bin/eplayer3 \
-	/bin/meta \
-	/lib/libeplayer3.s*
+	/usr/bin/eplayer3 \
+	/usr/bin/meta \
+	/usr/lib/libeplayer3.s*
 
 
 call[[ base_tdt_tools ]]
