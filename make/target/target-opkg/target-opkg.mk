207db097cf3b4c7520f8ec35e03eb3899d958614
Taapat <taapat@gmail.com>
2014-11-15 01:20:48 +0200

    Update opkg to 0.2.3, add patches from openpli

--- make/target/target-opkg/target-opkg.mk
+++ make/target/target-opkg/target-opkg.mk
@@ -5,14 +5,17 @@ package[[ target_opkg
 
 BDEPENDS_${P} = $(target_glibc) $(target_zlib)
 
-PV_${P} = 0.2.0
+PV_${P} = 0.2.3
 PR_${P} = 1
 PACKAGE_ARCH_${P} = $(box_arch)
 
 call[[ base ]]
 
 rule[[
-  extract:http://${PN}.googlecode.com/files/${PN}-${PV}.tar.gz
+  extract:http://downloads.yoctoproject.org/releases/${PN}/${PN}-${PV}.tar.gz
+  patch:file://${PN}.patch
+  install:-d:$(PKDIR)/usr/share/opkg/intercept
+  install_file:$(PKDIR)/usr/share/opkg/intercept/:file://modprobe
 ]]rule
 
 CONFIG_FLAGS_${P} = \
@@ -50,7 +53,6 @@ $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 	ln -sf opkg-cl $(PKDIR)/usr/bin/opkg
 	ln -sf opkg-cl $(PKDIR)/usr/bin/ipkg-cl
 	ln -sf opkg-cl $(PKDIR)/usr/bin/ipkg
-	
 	touch $@
 
 NAME_${P} = ${PN}
@@ -58,6 +60,7 @@ DESCRIPTION_${P} = lightweight package management system
 FILES_${P} = \
 	/etc/opkg \
 	/usr/bin \
+	/usr/share/opkg/intercept/modprobe \
 	/usr/lib/libopkg.so.*
 
 call[[ ipkbox ]]
