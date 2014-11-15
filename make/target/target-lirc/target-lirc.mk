67ed949651b65550c70977181c589d4b7ddcb72f
Taapat <taapat@gmail.com>
2014-11-15 02:02:18 +0200

    lirc: sync with own from amikoar-p

--- make/target/target-lirc/target-lirc.mk
+++ make/target/target-lirc/target-lirc.mk
@@ -14,11 +14,7 @@ call[[ base ]]
 
 rule[[
   extract:http://prdownloads.sourceforge.net/${PN}/${PN}-${PV}.tar.gz
-  patch:file://${PN}-neutrino-uinput-hack.diff
-  patch:file://${PN}-try_first_last_remote.diff
-  patch:file://${PN}-uinput-repeat-fix.diff
-  patch:file://${PN}-repeat_and_delay_hack.patch
-  patch:file://${PN}-rename_input_device.patch
+  patch:file://${PN}.diff
   nothing:file://lircd_hl101.conf
   nothing:file://lircd_hl101.conf.03_00_02
   nothing:file://lircd_hl101.conf.03_00_07
@@ -40,7 +36,7 @@ $(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
 	cd $(DIR_${P}) && \
 		$(BUILDENV) \
 		ac_cv_path_LIBUSB_CONFIG= \
-		CFLAGS="$(TARGET_CFLAGS) -Os -D__KERNEL_STRICT_NAMES -DUINPUT_NEUTRINO_HACK" \
+		CFLAGS="$(TARGET_CFLAGS) -Os -D__KERNEL_STRICT_NAMES" \
 		./configure \
 			--build=$(build) \
 			--host=$(target) \
