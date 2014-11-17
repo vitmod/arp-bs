b546de9900c3d940f34efc944203d59487f973e1
Taapat <taapat@gmail.com>
2014-11-18 01:34:06 +0200

    Change devinit location from /usr/bin to /bin

--- make/target/target-tdt-tools/target-devinit.mk
+++ make/target/target-tdt-tools/target-devinit.mk
@@ -8,7 +8,9 @@ BDEPENDS_${P} = $(target_glibc)
 PR_${P} = $(PR_tdt_tools).1
 
 DESCRIPTION_${P} = devinit
-FILES_${P} = /usr/bin/devinit
+FILES_${P} = /bin/devinit
+
+CONFIG_FLAGS_${P} += --prefix=/
 
 call[[ base_tdt_tools ]]
 
