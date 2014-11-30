5259d0bf521ac4d632fbac0389fd554fc4dbf13f
Taapat <taapat@gmail.com>
2014-11-30 21:48:38 +0200

    Use /proc/1/cmdline to check runlevel in update-rcd, thx Vasiliks & Igornorg

--- make/target/target-update-rcd/target-update-rcd.mk
+++ make/target/target-update-rcd/target-update-rcd.mk
@@ -6,7 +6,7 @@ package[[ target_update_rcd
 BDEPENDS_${P} = $(target_filesystem)
 
 PV_${P} = 0.1
-PR_${P} = 1
+PR_${P} = 2
 
 call[[ base ]]
 
@@ -14,6 +14,7 @@ rule[[
   nothing:git://github.com/philb/update-rc.d
   patch:file://update-rc-add-verbose.patch
   patch:file://update-rc-check-if-symlinks-are-valid.patch
+  patch:file://update-rc-use-cmdline.patch
 ]]rule
 
 $(TARGET_${P}).do_prepare: $(DEPENDS_${P})
