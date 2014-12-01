7c11f7098e22b2a6e4302d08543db7d8dfadb62c
schpuntik <schpuntik@freenet.de>
2014-12-01 23:38:11 +0100

    rename package python-livestreamer to livestreamer. Add python_requests python-futures and python-singledispatch to livestreamer rdepends Thx Taapat

--- make/target/target-python-livestreamer/target-python-livestreamer.mk
+++ make/target/target-python-livestreamer/target-python-livestreamer.mk
@@ -6,7 +6,7 @@ package[[ target_python_livestreamer
 BDEPENDS_${P} = $(target_python_setuptools)
 
 PV_${P} = 1.10.2
-PR_${P} = 1
+PR_${P} = 2
 
 DIR_${P} = $(WORK_${P})/livestreamer-${PV}
 
@@ -30,11 +30,11 @@ $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 	touch $@
 call[[ ipk ]]
 
-
+NAME_${P} = livestreamer
 DESCRIPTION_${P} = Livestreamer is command-line utility that extracts streams from various services and pipes them into a video player of choice.
 LICENSE_${P} = Take the official web page
 HOMEPAGE_${P} = http://livestreamer.tanuki.se/
-RDEPENDS_${P} = python_core libc6
+RDEPENDS_${P} = python_core python_requests python_futures python_singledispatch
 FILES_${P} = \
 $(PYTHON_DIR)/site-packages/livestreamer/*
 
