c4fe010f4ce4ff3670e3db1362fc6c4012aeec19
schpuntik <schpuntik@freenet.de>
2014-12-03 23:13:12 +0100

    Revert "Change devinit location from /usr/bin to /bin"
    This reverts commit e7bb2350f24e5210f4e86f7a2e593c79ee43ab21.

--- make/target/target-tdt-tools/target-devinit.mk
+++ make/target/target-tdt-tools/target-devinit.mk
@@ -8,9 +8,7 @@ BDEPENDS_${P} = $(target_glibc)
 PR_${P} = $(PR_tdt_tools).1
 
 DESCRIPTION_${P} = devinit
-FILES_${P} = /bin/devinit
-
-CONFIG_FLAGS_${P} += --prefix=/
+FILES_${P} = /usr/bin/devinit
 
 call[[ base_tdt_tools ]]
 
