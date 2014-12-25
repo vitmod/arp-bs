7e619ec644aec57864be3d621200352d06451b56
schpuntik <schpuntik@freenet.de>
2014-12-25 19:56:00 +0100

    rootfs update

--- make/target/target-rootfs/target-rootfs.mk
+++ make/target/target-rootfs/target-rootfs.mk
@@ -3,64 +3,11 @@
 #
 package[[ target_rootfs
 
-IPKBOX_LIST_${P} = $(target_glibc) $(target_gcc) $(target_gcc_lib) $(target_base_files) $(target_driver) $(target_netbase) $(target_opkg) $(target_busybox) $(target_base_passwd) \
+BDEPENDS_${P} = $(target_glibc) $(target_gcc) $(target_gcc_lib) $(target_base_files) $(target_driver) $(target_netbase) $(target_opkg) $(target_busybox) $(target_base_passwd) \
 $(target_ustslave) $(target_sysvinit) $(target_devinit) $(target_udev) $(target_lirc) $(target_vsftpd) $(target_ethtool) $(target_fonts) \
 $(target_fp_control) $(target_stfbcontrol) $(target_libfribidi) $(target_showiframe) $(target_portmap) $(target_firmware) $(target_bootelf) $(target_util_linux) $(target_e2fsprogs) $(target_wget) \
 $(target_udev_rules) $(target_bootlogo) $(target_flash_tools) $(target_rfkill) $(target_distro_feed_configs) $(target_initscripts) $(target_update_rcd) \
 $(target_streamripper)
-
-PV_${P} = 0.1
-PR_${P} = 6
-
-RM_WORK_${P} = $(false)
-call[[ base ]]
-#overwrite workdir
-WORK_${P} = $(prefix)/release
-DIR_${P} = $(WORK_${P})
-
-call[[ base_do_prepare ]]
-opkg_rootfs := opkg -f $(prefix)/opkg-box.conf -o $(DIR_${P})
-
-$(TARGET_${P}).do_install: $(TARGET_${P}).do_prepare
-	( echo "dest root /"; \
-	  echo "arch $(box_arch) 16"; \
-	  echo "arch sh4 10"; \
-	  echo "arch all 1"; \
-	  echo "src/gz box file://$(ipkbox)"; \
-	) > $(prefix)/opkg-box.conf
-
-	cd $(ipkbox) && \
-		/usr/bin/python $(hostprefix)/bin/ipkg-make-index . > Packages && \
-		cat Packages | gzip > Packages.gz
-	install -d $(DIR_${P})/usr/lib/opkg
-
-	export OPKG_OFFLINE_ROOT=$(DIR_${P}) && \
-	$(opkg_rootfs) update && \
-	$(opkg_rootfs) install --force-postinstall $(opkg_my_list)
-#	add version
-	echo "version=OpenAR-P_`date +%d-%m-%y-%T`_git-`git rev-list --count HEAD`" > $(DIR_${P})/etc/image-version
-	echo "----------------------------------------------------------" >>          $(DIR_${P})/etc/image-version
-	echo "----------------------------------------------------------" >>          $(DIR_${P})/etc/image-version
-	cat $(buildprefix)/.config |grep -v '^#' |tr ' ' '\n' >>                      $(DIR_${P})/etc/image-version
-	echo "OpenAR-P \n \l" > $(DIR_${P})/etc/issue
-	touch $@
-
-$(TARGET_${P}): $(TARGET_${P}).do_install
-
-# helps to fill DEPENDS list
-$(TARGET_${P}).print_depends:
-#	catch cat exitstatus and see stderr
-	@cd $(ipkorigin) && cat $(addsuffix .origin,$(opkg_my_list)) 2>&1 |uniq
-
-# some packages are installed due to rdepends of other ones
-# we want to see all the installed packages and rebuild rootfs when any of them changes
-$(TARGET_${P}).print_depends_all: $(TARGET_${P})
-	set -e; \
-	list=`$(opkg_rootfs) list-installed | cut -d ' ' -f 1`; \
-	for x in $${list}; do \
-		cat $(ipkorigin)/$${x}.origin || true; \
-	done
-
 
 # core system libraries, binaries and scripts
 opkg_my_list = \
@@ -119,7 +66,7 @@ opkg_my_list += \
 #extras
 
 ifdef CONFIG_WLAN_SUPPORT
-IPKBOX_LIST_${P} += $(target_wireless_tools) $(target_firmware_wlan) $(target_compat_wireless)
+BDEPENDS_${P} += $(target_wireless_tools) $(target_firmware_wlan) $(target_compat_wireless)
 opkg_my_list += \
 	wireless-tools \
 	kernel-module-rt2870sta \
@@ -132,22 +79,22 @@ opkg_my_list += \
 	kernel-module-rt73usb
 endif
 ifdef CONFIG_3G_SUPPORT
-IPKBOX_LIST_${P} += $(target_modem_scripts) $(target_usb_modeswitch) $(target_pppd)
+BDEPENDS_${P} += $(target_modem_scripts) $(target_usb_modeswitch) $(target_pppd)
 opkg_my_list += modem-scripts
 endif
 ifdef CONFIG_NTFS_3G_SUPPORT
-IPKBOX_LIST_${P} += $(target_ntfs_3g)
+BDEPENDS_${P} += $(target_ntfs_3g)
 opkg_my_list += ntfs-3g
 endif
 ifdef CONFIG_IPTABLES_SUPPORT
-IPKBOX_LIST_${P} += $(target_iptables)
+BDEPENDS_${P} += $(target_iptables)
 opkg_my_list += iptables
 endif
 
 #########################################################################################
 # enigma2
 ifdef CONFIG_BUILD_ENIGMA2
-IPKBOX_LIST_${P} += $(target_enigma2) $(target_tuxbox_configs) $(target_hotplug_e2_helper) $(target_python_pycrypto)
+BDEPENDS_${P} += $(target_enigma2) $(target_python) $(target_tuxbox_configs) $(target_hotplug_e2_helper) $(target_python_pycrypto)
 opkg_my_list += \
 	config-satellites \
 	config-cables \
@@ -186,21 +133,21 @@ opkg_my_list += \
 	enigma2-plugin-systemplugins-uniontunertype
 endif
 ifdef CONFIG_WLAN_SUPPORT
-IPKBOX_LIST_${P} += $(target_python_wifi)
+BDEPENDS_${P} += $(target_python_wifi)
 opkg_my_list += enigma2-plugin-systemplugins-wirelesslan
 endif
 endif
 
 ifdef CONFIG_ENIGMA2_PLUGINS
-IPKBOX_LIST_${P} += $(target_enigma2_plugins)
+BDEPENDS_${P} += $(target_enigma2_plugins)
 endif
 
 ifdef CONFIG_ENIGMA2_SKINS
-IPKBOX_LIST_${P} += $(target_enigma2_skins)
+BDEPENDS_${P} += $(target_enigma2_skins)
 endif
 
 ifdef CONFIG_ENIGMA2_EXTENSION_OPENWEBIF
-IPKBOX_LIST_${P} += $(target_openwebif)
+BDEPENDS_${P} += $(target_openwebif)
 opkg_my_list += enigma2-plugin-extensions-openwebif
 endif
 
@@ -209,12 +156,12 @@ opkg_my_list += enigma2-plugin-extensions-webinterface
 endif
 
 ifdef CONFIG_ENIGMA2_EXTENSION_MEDIAPORTAL
-IPKBOX_LIST_${P} += $(target_mediaportal)
+BDEPENDS_${P} += $(target_mediaportal)
 opkg_my_list += enigma2-plugin-extensions-mediaportal
 endif
 
 ifdef CONFIG_PYTHON_LIVESTREAMER
-IPKBOX_LIST_${P} += $(target_python_livestreamer)
+BDEPENDS_${P} += $(target_python_livestreamer)
 opkg_my_list += livestreamer
 endif
 
@@ -223,17 +170,17 @@ opkg_my_list += enigma2-plugin-skin-megamod
 endif
 
 ifdef CONFIG_ENIGMA2_SKIN_MAGIC
-IPKBOX_LIST_${P} += $(target_enigma2_skins_magic)
+BDEPENDS_${P} += $(target_enigma2_skins_magic)
 opkg_my_list += enigma2-plugin-skin-magic
 endif
 
 ifdef CONFIG_ENIGMA2_SKIN_PLIHD
-IPKBOX_LIST_${P} += $(target_enigma2_skins_plihd)
+BDEPENDS_${P} += $(target_enigma2_skins_plihd)
 opkg_my_list += enigma2-plugin-skin-plihd
 endif
 
 ifdef CONFIG_ENIGMA2_SKIN_METROPOLISHD
-IPKBOX_LIST_${P} += $(target_enigma2_skins_metropolishd)
+BDEPENDS_${P} += $(target_enigma2_skins_metropolishd)
 opkg_my_list += enigma2-plugin-skin-metropolishd
 endif
 
@@ -242,7 +189,7 @@ opkg_my_list += enigma2-plugin-extensions-alternativesoftcammanager
 endif
 
 ifdef CONFIG_OSCAM
-IPKBOX_LIST_${P} += $(target_oscam)
+BDEPENDS_${P} += $(target_oscam)
 opkg_my_list += enigma2-plugin-cams-oscam
 endif
 
@@ -250,7 +197,7 @@ endif
 #neutrino
 
 ifdef CONFIG_BUILD_NEUTRINO
-IPKBOX_LIST_${P} += $(target_neutrino) $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad)
+BDEPENDS_${P} += $(target_neutrino) $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad)
 opkg_my_list += neutrino libblkid1
 endif
 
@@ -262,11 +209,63 @@ endif
 ########################################################################################
 #xbmc
 ifdef CONFIG_BUILD_XBMC
-IPKBOX_LIST_${P} += $(target_xbmc) $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad)
+BDEPENDS_${P} += $(target_xbmc) $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad)
 opkg_my_list += libblkid1
 endif
 
 #########################################################################################
+PV_${P} = 0.1
+PR_${P} = 7
+
+RM_WORK_${P} = $(false)
+
+call[[ base ]]
+#overwrite workdir
+WORK_${P} = $(prefix)/release
+DIR_${P} = $(WORK_${P})
+
+call[[ base_do_prepare ]]
+opkg_rootfs := opkg -f $(prefix)/opkg-box.conf -o $(DIR_${P})
+
+$(TARGET_${P}).do_install: $(TARGET_${P}).do_prepare
+	( echo "dest root /"; \
+	  echo "arch $(box_arch) 16"; \
+	  echo "arch sh4 10"; \
+	  echo "arch all 1"; \
+	  echo "src/gz box file://$(ipkbox)"; \
+	) > $(prefix)/opkg-box.conf
+
+	cd $(ipkbox) && \
+		/usr/bin/python $(hostprefix)/bin/ipkg-make-index . > Packages && \
+		cat Packages | gzip > Packages.gz
+	install -d $(DIR_${P})/usr/lib/opkg
+
+	export OPKG_OFFLINE_ROOT=$(DIR_${P}) && \
+	$(opkg_rootfs) update && \
+	$(opkg_rootfs) install --force-postinstall $(opkg_my_list)
+#	add version
+	echo "version=OpenAR-P_`date +%d-%m-%y-%T`_git-`git rev-list --count HEAD`" > $(DIR_${P})/etc/image-version
+	echo "----------------------------------------------------------" >>          $(DIR_${P})/etc/image-version
+	echo "----------------------------------------------------------" >>          $(DIR_${P})/etc/image-version
+	cat $(buildprefix)/.config |grep -v '^#' |tr ' ' '\n' >>                      $(DIR_${P})/etc/image-version
+	echo "OpenAR-P \n \l" > $(DIR_${P})/etc/issue
+	touch $@
+
+$(TARGET_${P}): $(TARGET_${P}).do_install
+
+# helps to fill DEPENDS list
+$(TARGET_${P}).print_depends:
+#	catch cat exitstatus and see stderr
+	@cd $(ipkorigin) && cat $(addsuffix .origin,$(opkg_my_list)) 2>&1 |uniq
+
+# some packages are installed due to rdepends of other ones
+# we want to see all the installed packages and rebuild rootfs when any of them changes
+$(TARGET_${P}).print_depends_all: $(TARGET_${P})
+	set -e; \
+	list=`$(opkg_rootfs) list-installed | cut -d ' ' -f 1`; \
+	for x in $${list}; do \
+		cat $(ipkorigin)/$${x}.origin || true; \
+	done
 
 
 ]]package
