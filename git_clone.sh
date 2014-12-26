09c08a8a046a654d6fbe435093895941282cdf0b
schpuntik <schpuntik@freenet.de>
2014-12-26 01:40:58 +0100

    add git clone script

---
+++ git_clone.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIR=`pwd`
+GITBRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
+REPLIST="apps driver"
+
+	for f in  $REPLIST ; do
+		if [ -d "$f" ]; then
+			echo "$f already cloned"
+		else
+			if [ "$2" = "dev" ]; then
+				# dev
+				git clone git@github.com:OpenAR-P/$f $f; cd $f; git checkout $GITBRANCH; cd $DIR
+			else
+				# usr
+				git clone git://github.com/OpenAR-P/$f $f; cd $f; git checkout $GITBRANCH; cd $DIR
+			fi
+			echo "git clone" $f
+		fi
+	done
