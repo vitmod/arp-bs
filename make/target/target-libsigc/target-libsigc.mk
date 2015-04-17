7d85bc6a13cf2f982da08e76caf12b1805275e80
schpuntik <schpuntik@freenet.de>
2015-04-17 23:50:39 +0200

    libsigc add 2.3 version build

--- make/target/target-libsigc/target-libsigc.mk
+++ make/target/target-libsigc/target-libsigc.mk
@@ -5,16 +5,29 @@ package[[ target_libsigc
 
 BDEPENDS_${P} = $(target_gcc_lib)
 
+ifdef CONFIG_BUILD_NEUTRINO
+PV_${P} = 2.3.2
+PR_${P} = 1
+DV_${P} = 2.3
+else
 PV_${P} = 1.2.7
 PR_${P} = 1
+DV_${P} = 1.2
+endif
+
 
 DIR_${P} = $(WORK_${P})/libsigc++-${PV}
 
 call[[ base ]]
 
 rule[[
-  extract:http://ftp.gnome.org/pub/GNOME/sources/libsigc++/1.2/libsigc++-${PV}.tar.gz
+ifdef CONFIG_BUILD_NEUTRINO
+  extract:http://ftp.gnome.org/pub/GNOME/sources/libsigc++/${DV}/libsigc++-${PV}.tar.xz
+else
+  extract:http://ftp.gnome.org/pub/GNOME/sources/libsigc++/${DV}/libsigc++-${PV}.tar.gz
+endif
 ]]rule
+
 
 $(TARGET_${P}).do_prepare: $(DEPENDS_${P})
 	$(PREPARE_${P})
@@ -39,7 +52,7 @@ $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 
 call[[ ipk ]]
 
-NAME_${P} = libsigc-1.2
+NAME_${P} = libsigc-${DV}
 DESCRIPTION_${P} =  A library for loose coupling of C++ method calls
 RDEPENDS_${P} = libstdc++6 libgcc1
 FILES_${P} = /usr/lib/*.so.*
