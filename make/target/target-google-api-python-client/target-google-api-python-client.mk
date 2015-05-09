a5a9d1d18c9ee2fb4fff783a8663ef94cf4682e7
Taapat <taapat@gmail.com>
2015-05-09 19:35:09 +0300

    Add google-api-python-client

---
+++ make/target/target-google-api-python-client/target-google-api-python-client.mk
@@ -0,0 +1,42 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_google_api_python_client
+
+BDEPENDS_${P} = $(target_python_setuptools)
+
+PV_${P} = git
+PR_${P} = 1
+PACKAGE_ARCH_${P} = all
+
+call[[ base ]]
+
+rule[[
+  git://github.com/google/${PN}.git
+]]rule
+
+call[[ git ]]
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && $(python_build)
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && $(python_install)
+	touch $@
+
+
+DESCRIPTION_${P} = "Python client library for Google's discovery based APIs."
+RDEPENDS_${P} = python_core python_six python_oauth2client python_httplib2 python_uritemplate
+FILES_${P} =   \
+$(PYTHON_DIR)/site-packages/apiclient/* \
+$(PYTHON_DIR)/site-packages/googleapiclient/*
+
+call[[ ipkbox ]]
+
+]]package
