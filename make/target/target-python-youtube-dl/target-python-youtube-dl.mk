95577467377d6bf1c20e68abfcfa0403fa917da0
Taapat <taapat@gmail.com>
2015-05-10 13:52:16 +0300

    Leave only youtube extractor in python-youtube-dl to reduce the packet size

--- make/target/target-python-youtube-dl/target-python-youtube-dl.mk
+++ make/target/target-python-youtube-dl/target-python-youtube-dl.mk
@@ -6,19 +6,21 @@ package[[ target_python_youtube_dl
 BDEPENDS_${P} = $(target_python_setuptools)
 
 PV_${P} = git
-PR_${P} = 1
+PR_${P} = 2
 PACKAGE_ARCH_${P} = all
 
 call[[ base ]]
 
 rule[[
   git://github.com/rg3/youtube-dl.git
+  nothing:file://extractor__init__.py
 ]]rule
 
 call[[ git ]]
 
 $(TARGET_${P}).do_prepare: $(DEPENDS_${P})
 	$(PREPARE_${P})
+	cd $(DIR_${P}) && cp -f extractor__init__.py youtube_dl/extractor/__init__.py
 	touch $@
 
 $(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
@@ -35,7 +37,12 @@ $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 DESCRIPTION_${P} = "Small command-line program to download videos from YouTube.com and other video sites"
 RDEPENDS_${P} = python_core python_unixadmin python_html python_ctypes python_email
 FILES_${P} =   \
-$(PYTHON_DIR)/site-packages/youtube_dl/* \
+$(PYTHON_DIR)/site-packages/youtube_dl/*.p* \
+$(PYTHON_DIR)/site-packages/youtube_dl/downloader/* \
+$(PYTHON_DIR)/site-packages/youtube_dl/postprocessor/* \
+$(PYTHON_DIR)/site-packages/youtube_dl/extractor/__init__.p* \
+$(PYTHON_DIR)/site-packages/youtube_dl/extractor/common.p* \
+$(PYTHON_DIR)/site-packages/youtube_dl/extractor/youtube.p* \
 /usr/bin/youtube-dl
 
 call[[ ipkbox ]]
