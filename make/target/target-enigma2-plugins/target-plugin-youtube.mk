64aad571f5ca4db8db5a3219dff56e3f745a38df
Taapat <taapat@gmail.com>
2015-07-06 00:27:38 +0300

    Add target-plugin-youtube

---
+++ make/target/target-enigma2-plugins/target-plugin-youtube.mk
@@ -0,0 +1,41 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_plugin_youtube
+
+PV_${P} = git
+PR_${P} = 1
+PACKAGE_ARCH_${P} = all
+
+call[[ base ]]
+
+rule[[
+  git://github.com/Taapat/enigma2-plugin-youtube.git:b=master
+]]rule
+
+call[[ git ]]
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && \
+	$(crossprefix)/bin/python ./setup.py build
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && \
+	$(crossprefix)/bin/python ./setup.py install --install-purelib=$(PKDIR)/usr/lib/enigma2/python/Plugins/Extensions
+	touch $@
+
+NAME_${P} = enigma2-plugin-extensions-youtube
+DESCRIPTION_${P} = Watch YouTube videos
+RDEPENDS_${P} = python_core python-twisted-web google-api-python-client python-youtube-dl
+
+FILES_${P} = /usr/lib/enigma2/python/Plugins/Extensions/YouTube/*
+
+call[[ ipkbox ]]
+
+]]package
