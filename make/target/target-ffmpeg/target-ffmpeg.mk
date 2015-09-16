b8e9dc453829ab34a0eaf6111ef8d5225ae1fb51
schpuntik <schpuntik@freenet.de>
2015-09-16 20:03:27 +0200

    change config ffmpeg

--- make/target/target-ffmpeg/target-ffmpeg.mk
+++ make/target/target-ffmpeg/target-ffmpeg.mk
@@ -22,14 +22,26 @@ rule[[
   patch:file://${PN}-aac-${PV_MOJ}.patch
 ]]rule
 
+FFMPEG_FLAGS_${P} = \
+		--enable-librtmp \
+		--enable-protocol=librtmp \
+		--enable-protocol=librtmpe \
+		--enable-protocol=librtmps \
+		--enable-protocol=librtmpt \
+		--enable-protocol=librtmpte
+
 CONFIG_FLAGS_${P} = \
-		--disable-static \
-		--enable-shared \
-		--disable-runtime-cpudetect \
 		--disable-ffserver \
 		--disable-ffplay \
-		--disable-ffprobe \
+		--disable-ffprobe
+CONFIG_FLAGS_${P} += \
 		--disable-doc \
+		--disable-htmlpages \
+		--disable-manpages \
+		--disable-podpages \
+		--disable-txtpages
+CONFIG_FLAGS_${P} += \
+		--disable-asm \
 		--disable-altivec \
 		--disable-amd3dnow \
 		--disable-amd3dnowext \
@@ -42,10 +54,7 @@ CONFIG_FLAGS_${P} = \
 		--disable-sse4 \
 		--disable-sse42 \
 		--disable-avx \
-		--disable-xop \
-		--disable-fma3 \
 		--disable-fma4 \
-		--disable-avx2 \
 		--disable-armv5te \
 		--disable-armv6 \
 		--disable-armv6t2 \
@@ -58,154 +67,147 @@ CONFIG_FLAGS_${P} = \
 		--disable-mipsdspr2 \
 		--disable-mipsfpu \
 		--disable-fast-unaligned \
+		--disable-dxva2 \
+		--disable-vaapi \
+		--disable-vdpau
+CONFIG_FLAGS_${P} += \
 		--disable-muxers \
+		--enable-muxer=flac \
+		--enable-muxer=mp3 \
+		--enable-muxer=h261 \
+		--enable-muxer=h263 \
+		--enable-muxer=h264 \
+		--enable-muxer=image2 \
+		--enable-muxer=mpeg1video \
+		--enable-muxer=mpeg2video \
+		--enable-muxer=ogg
+CONFIG_FLAGS_${P} += \
+		--disable-parsers \
+		--enable-parser=aac \
+		--enable-parser=aac_latm \
+		--enable-parser=ac3 \
+		--enable-parser=dca \
+		--enable-parser=dvbsub \
+		--enable-parser=dvdsub \
+		--enable-parser=flac \
+		--enable-parser=h264 \
+		--enable-parser=mjpeg \
+		--enable-parser=mpeg4video \
+		--enable-parser=mpegvideo \
+		--enable-parser=mpegaudio \
+		--enable-parser=vc1 \
+		--enable-parser=vorbis
+CONFIG_FLAGS_${P} += \
 		--disable-encoders \
+		--enable-encoder=aac \
+		--enable-encoder=h261 \
+		--enable-encoder=h263 \
+		--enable-encoder=h263p \
+		--enable-encoder=ljpeg \
+		--enable-encoder=mjpeg \
+		--enable-encoder=mpeg1video \
+		--enable-encoder=mpeg2video \
+		--enable-encoder=png
+CONFIG_FLAGS_${P} += \
 		--disable-decoders \
 		--enable-decoder=aac \
-		--enable-decoder=aac_latm \
-		--enable-decoder=adpcm_4xm \
-		--enable-decoder=adpcm_adx \
-		--enable-decoder=adpcm_afc \
-		--enable-decoder=adpcm_ct \
-		--enable-decoder=adpcm_dtk \
-		--enable-decoder=adpcm_ea \
-		--enable-decoder=adpcm_ea_maxis_xa \
-		--enable-decoder=adpcm_ea_r1 \
-		--enable-decoder=adpcm_ea_r2 \
-		--enable-decoder=adpcm_ea_r3 \
-		--enable-decoder=adpcm_ea_xas \
-		--enable-decoder=adpcm_g722 \
-		--enable-decoder=adpcm_g726 \
-		--enable-decoder=adpcm_g726le \
-		--enable-decoder=adpcm_ima_amv \
-		--enable-decoder=adpcm_ima_apc \
-		--enable-decoder=adpcm_ima_dk3 \
-		--enable-decoder=adpcm_ima_dk4 \
-		--enable-decoder=adpcm_ima_ea_eacs \
-		--enable-decoder=adpcm_ima_ea_sead \
-		--enable-decoder=adpcm_ima_iss \
-		--enable-decoder=adpcm_ima_oki \
-		--enable-decoder=adpcm_ima_qt \
-		--enable-decoder=adpcm_ima_rad \
-		--enable-decoder=adpcm_ima_smjpeg \
-		--enable-decoder=adpcm_ima_wav \
-		--enable-decoder=adpcm_ima_ws \
-		--enable-decoder=adpcm_ms \
-		--enable-decoder=adpcm_sbpro_2 \
-		--enable-decoder=adpcm_sbpro_3 \
-		--enable-decoder=adpcm_sbpro_4 \
-		--enable-decoder=adpcm_swf \
-		--enable-decoder=adpcm_thp \
-		--enable-decoder=adpcm_vima \
-		--enable-decoder=adpcm_xa \
-		--enable-decoder=adpcm_yamaha \
-		--enable-decoder=alac \
-		--enable-decoder=ape \
-		--enable-decoder=atrac1 \
-		--enable-decoder=atrac3 \
-		--enable-decoder=atrac3p \
-		--enable-decoder=binkaudio_dct \
-		--enable-decoder=binkaudio_rdft \
-		--enable-decoder=bmv_audio \
-		--enable-decoder=comfortnoise \
-		--enable-decoder=cook \
-		--enable-decoder=dsd_lsbf \
-		--enable-decoder=dsd_lsbf_planar \
-		--enable-decoder=dsd_msbf \
-		--enable-decoder=dsd_msbf_planar \
-		--enable-decoder=dsicinaudio \
-		--enable-decoder=eac3 \
-		--enable-decoder=evrc \
+		--enable-decoder=dca \
+		--enable-decoder=dvbsub \
+		--enable-decoder=dvdsub \
 		--enable-decoder=flac \
-		--enable-decoder=g723_1 \
-		--enable-decoder=g729 \
-		--enable-decoder=gsm \
-		--enable-decoder=gsm_ms \
-		--enable-decoder=iac \
-		--enable-decoder=imc \
-		--enable-decoder=interplay_dpcm \
-		--enable-decoder=mace3 \
-		--enable-decoder=mace6 \
-		--enable-decoder=metasound \
-		--enable-decoder=mlp \
-		--enable-decoder=mp1 \
-		--enable-decoder=mp3adu \
-		--enable-decoder=mp3on4 \
-		--enable-decoder=nellymoser \
-		--enable-decoder=opus \
-		--enable-decoder=paf_audio \
-		--enable-decoder=pcm_alaw \
-		--enable-decoder=pcm_bluray \
-		--enable-decoder=pcm_dvd \
-		--enable-decoder=pcm_f32be \
-		--enable-decoder=pcm_f32le \
-		--enable-decoder=pcm_f64be \
-		--enable-decoder=pcm_f64le \
-		--enable-decoder=pcm_lxf \
-		--enable-decoder=pcm_mulaw \
-		--enable-decoder=pcm_s16be \
-		--enable-decoder=pcm_s16be_planar \
+		--enable-decoder=h261 \
+		--enable-decoder=h263 \
+		--enable-decoder=h263i \
+		--enable-decoder=h264 \
+		--enable-decoder=mjpeg \
+		--enable-decoder=mp3 \
+		--enable-decoder=mpeg1video \
+		--enable-decoder=mpeg2video \
+		--enable-decoder=msmpeg4v1 \
+		--enable-decoder=msmpeg4v2 \
+		--enable-decoder=msmpeg4v3 \
 		--enable-decoder=pcm_s16le \
+		--enable-decoder=pcm_s16be \
 		--enable-decoder=pcm_s16le_planar \
-		--enable-decoder=pcm_s24be \
-		--enable-decoder=pcm_s24daud \
-		--enable-decoder=pcm_s24le \
-		--enable-decoder=pcm_s24le_planar \
-		--enable-decoder=pcm_s32be \
-		--enable-decoder=pcm_s32le \
-		--enable-decoder=pcm_s32le_planar \
-		--enable-decoder=pcm_s8 \
-		--enable-decoder=pcm_s8_planar \
-		--enable-decoder=pcm_u16be \
-		--enable-decoder=pcm_u16le \
-		--enable-decoder=pcm_u24be \
-		--enable-decoder=pcm_u24le \
-		--enable-decoder=pcm_u32be \
-		--enable-decoder=pcm_u32le \
-		--enable-decoder=pcm_u8 \
-		--enable-decoder=pcm_zork \
-		--enable-decoder=qcelp \
-		--enable-decoder=qdm2 \
-		--enable-decoder=ra_144 \
-		--enable-decoder=ra_288 \
-		--enable-decoder=ralf \
-		--enable-decoder=roq_dpcm \
-		--enable-decoder=s302m \
-		--enable-decoder=shorten \
-		--enable-decoder=sipr \
-		--enable-decoder=sol_dpcm \
-		--enable-decoder=sonic \
-		--enable-decoder=tak \
-		--enable-decoder=truehd \
-		--enable-decoder=truespeech \
-		--enable-decoder=tta \
-		--enable-decoder=twinvq \
-		--enable-decoder=vima \
-		--enable-decoder=vmdaudio \
+		--enable-decoder=pcm_s16be_planar \
+		--enable-decoder=pgssub \
+		--enable-decoder=png \
+		--enable-decoder=srt \
+		--enable-decoder=subrip \
+		--enable-decoder=subviewer \
+		--enable-decoder=subviewer1 \
+		--enable-decoder=text \
+		--enable-decoder=theora \
 		--enable-decoder=vorbis \
-		--enable-decoder=wavpack \
-		--enable-decoder=wmalossless \
-		--enable-decoder=wmapro \
-		--enable-decoder=wmav1 \
-		--enable-decoder=wmav2 \
-		--enable-decoder=wmavoice \
-		--enable-decoder=xan_dpcm \
+		--enable-decoder=wmv3 \
+		--enable-decoder=xsub
+CONFIG_FLAGS_${P} += \
+		--disable-demuxers \
+		--enable-demuxer=aac \
+		--enable-demuxer=ac3 \
+		--enable-demuxer=avi \
+		--enable-demuxer=dts \
+		--enable-demuxer=flac \
+		--enable-demuxer=flv \
+		--enable-demuxer=hds \
+		--enable-demuxer=hls \
+		--enable-demuxer=image* \
+		--enable-demuxer=matroska \
+		--enable-demuxer=mjpeg \
+		--enable-demuxer=mov \
+		--enable-demuxer=mp3 \
+		--enable-demuxer=mpegts \
+		--enable-demuxer=mpegtsraw \
+		--enable-demuxer=mpegps \
+		--enable-demuxer=mpegvideo \
+		--enable-demuxer=ogg \
+		--enable-demuxer=pcm_s16be \
+		--enable-demuxer=pcm_s16le \
+		--enable-demuxer=rm \
+		--enable-demuxer=rtp \
+		--enable-demuxer=rtsp \
+		--enable-demuxer=srt \
+		--enable-demuxer=vc1 \
+		--enable-demuxer=wav
+CONFIG_FLAGS_${P} += \
+		--disable-protocols \
+		--enable-protocol=file \
+		--enable-protocol=http \
+		--enable-protocol=mmsh \
+		--enable-protocol=mmst \
+		--enable-protocol=rtmp \
+		--enable-protocol=rtmpe \
+		--enable-protocol=rtmps \
+		--enable-protocol=rtmpt \
+		--enable-protocol=rtmpte \
+		--enable-protocol=rtmpts
+CONFIG_FLAGS_${P} += \
+		--disable-filters \
+		--enable-filter=scale
+CONFIG_FLAGS_${P} += \
+		--disable-postproc \
+		--disable-bsfs \
+		--disable-indevs \
+		--disable-outdevs \
+		--enable-bzlib \
+		--enable-zlib \
+		$(FFMPEG_FLAGS)
+CONFIG_FLAGS_${P} += \
+		--disable-static \
+		--enable-shared \
+		--enable-small \
 		--enable-libbluray \
 		--enable-protocol=bluray \
 		--enable-nonfree \
 		--enable-openssl \
-		--disable-indevs \
-		--disable-outdevs \
-		--enable-bzlib \
-		--disable-zlib \
-		--disable-bsfs \
-		--enable-librtmp \
+		--enable-stripping \
+		--disable-debug \
+		--disable-runtime-cpudetect \
 		--pkg-config="pkg-config" \
 		--enable-cross-compile \
 		--cross-prefix=$(target)- \
 		--target-os=linux \
 		--arch=sh4 \
-		--disable-debug \
 		--enable-pthreads \
 		--prefix=/usr
 
