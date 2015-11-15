3f76646cd6711b6b16befd82b702b0f6b1fa681b
sid8796 <sid8796@gmail.com>
2015-11-15 11:05:15 +0200

    Add plugin AlternativeSoftCamManager from taapat repo

---
+++ make/target/target-enigma2-plugins/target-plugin-alternativesoftcammanager.mk
@@ -0,0 +1,42 @@
+#
+# AR-P buildsystem smart Makefile
+#
+package[[ target_plugin_alternativesoftcammanager
+
+PV_${P} = git
+PR_${P} = 1
+PACKAGE_ARCH_${P} = all
+
+call[[ base ]]
+
+rule[[
+  git://github.com/Taapat/enigma2-plugin-alternativesoftcammanager.git:b=master
+]]rule
+
+call[[ git ]]
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && \
+	$(crossprefix)/bin/python ./setup.py build
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && \
+	$(crossprefix)/bin/python ./setup.py install \
+	--install-purelib=$(PKDIR)/usr/lib/enigma2/python/Plugins
+	touch $@
+
+NAME_${P} = enigma2-plugin-extensions-alternativesoftcammanager
+DESCRIPTION_${P} = Start, stop, restart SoftCams, change settings
+MAINTAINER_${P} = Taapat taapat@gmail.com
+
+FILES_${P} = /usr/lib/enigma2/python/Plugins/Extensions/AlternativeSoftCamManager/*
+
+call[[ ipkbox ]]
+
+]]package
