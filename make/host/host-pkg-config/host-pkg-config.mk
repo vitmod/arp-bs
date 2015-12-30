63c7b8dcc2b3690cdde914774b392cc1f14f6c83
schpuntik <schpuntik@freenet.de>
2015-12-30 17:53:19 +0100

    update host-pkg-config to 0.28

--- make/host/host-pkg-config/host-pkg-config.mk
+++ make/host/host-pkg-config/host-pkg-config.mk
@@ -5,17 +5,36 @@ package[[ host_pkg_config
 
 BDEPENDS_${P} = $(host_opkg_meta) $(host_rpmconfig) $(host_autotools)
 
+PV_${P} = 0.28
 PR_${P} = 1
 
-PV_${P} = 0.23-3
-${P}_SPEC = stm-$(${P}).spec
-${P}_SPEC_PATCH = 
-${P}_PATCHES = 
-${P}_SRCRPM = $(archivedir)/$(STLINUX)-$(${P})-$(PV_${P}).src.rpm
-
 call[[ base ]]
-call[[ base_rpm ]]
+
+rule[[
+  extract:http://pkgconfig.freedesktop.org/releases/${PN}-${PV}.tar.gz
+  patch:file://${PN}-${PV}.patch
+]]rule
+
+$(TARGET_${P}).do_prepare: $(DEPENDS_${P})
+	$(PREPARE_${P})
+	touch $@
+
+$(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
+	cd $(DIR_${P}) && \
+		./configure \
+			--prefix=$(hostprefix) \
+			--disable-host-tool \
+			--with-pc_path=$(targetprefix)/usr/lib/pkgconfig \
+		&& \
+		$(MAKE)
+	touch $@
+
+$(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
+	$(PKDIR_clean)
+	cd $(DIR_${P}) && \
+		make install DESTDIR=$(PKDIR)
+	touch $@
+
 call[[ ipk ]]
-call[[ rpm ]]
 
 ]]package
