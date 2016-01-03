34d2e81c82741688fc9a382f7ace27cef7df919b
schpuntik <schpuntik@freenet.de>
2016-01-04 00:37:16 +0100

    back to locale  fonts

--- make/target/target-fonts/target-fonts.mk
+++ make/target/target-fonts/target-fonts.mk
@@ -6,7 +6,7 @@ package[[ target_fonts
 BDEPENDS_${P} = $(target_filesystem)
 
 PV_${P} = 0.1
-PR_${P} = 4
+PR_${P} = 5
 PACKAGE_ARCH_${P} = all
 
 DESCRIPTION_${P} = fonts
@@ -16,6 +16,7 @@ call[[ base ]]
 rule[[
   pdircreate:${PN}-${PV}
   install:-d:$(PKDIR)/usr/share/fonts
+  install_file:$(PKDIR)/usr/share/fonts:file://ae_AlMateen.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://allruk.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://allruf.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://allru.ttf
@@ -24,6 +25,9 @@ rule[[
   install_file:$(PKDIR)/usr/share/fonts:file://ds_digital.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://FreeSans.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://goodtime.ttf
+  install_file:$(PKDIR)/usr/share/fonts:file://lcd.ttf
+  install_file:$(PKDIR)/usr/share/fonts:file://md_khmurabi_10.ttf
+  install_file:$(PKDIR)/usr/share/fonts:file://nmsbd.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://seg_internat.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://seg.ttf
   install_file:$(PKDIR)/usr/share/fonts:file://Symbols.ttf
@@ -45,10 +49,12 @@ $(TARGET_${P}).do_package: $(TARGET_${P}).do_prepare
 	touch $@
 
 PACKAGES_${P} = \
+	font_ae_almateen \
 	font_allruk \
 	font_blue \
 	font_ds_digital \
 	font_goodtime \
+	font_md_khmurabi \
 	font_seg_internat \
 	font_symbols \
 	font_uhr_digital \
@@ -57,16 +63,20 @@ PACKAGES_${P} = \
 	font_allru \
 	font_dejavulgcsans_bold \
 	font_freesans \
+	font_lcd \
+	font_nmsbd \
 	font_seg \
 	font_tuxtxt \
 	font_valis_enigma
 
 DESCRIPTION_${P} = ttf fonts
 
+FILES_font_ae_almateen = /usr/share/fonts/ae_AlMateen.ttf
 FILES_font_allruk = /usr/share/fonts/allruk.ttf
 FILES_font_blue = /usr/share/fonts/blue.ttf
 FILES_font_ds_digital = /usr/share/fonts/ds_digital.ttf
 FILES_font_goodtime = /usr/share/fonts/goodtime.ttf
+FILES_font_md_khmurabi = /usr/share/fonts/md_khmurabi_10.ttf
 FILES_font_seg_internat = /usr/share/fonts/seg_internat.ttf
 FILES_font_symbols = /usr/share/fonts/Symbols.ttf
 FILES_font_uhr_digital = /usr/share/fonts/uhr-digital.ttf
@@ -75,6 +85,8 @@ FILES_font_allruf = /usr/share/fonts/allruf.ttf
 FILES_font_allru = /usr/share/fonts/allru.ttf
 FILES_font_dejavulgcsans_bold = /usr/share/fonts/DejaVuLGCSans-Bold.ttf
 FILES_font_freesans = /usr/share/fonts/FreeSans.ttf
+FILES_font_lcd = /usr/share/fonts/lcd.ttf
+FILES_font_nmsbd = /usr/share/fonts/nmsbd.ttf
 FILES_font_seg = /usr/share/fonts/seg.ttf
 FILES_font_tuxtxt = /usr/share/fonts/tuxtxt.ttf /usr/share/fonts/tuxtxt.otb
 FILES_font_valis_enigma = /usr/share/fonts/valis_enigma.ttf
