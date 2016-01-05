859121e4c7147082cdc4860b7925eaec307de255
schpuntik <schpuntik@freenet.de>
2016-01-05 16:59:46 +0100

    add tvheadend to release build

--- make/target/target-rootfs/target-rootfs.mk
+++ make/target/target-rootfs/target-rootfs.mk
@@ -231,8 +231,8 @@ endif
 ########################################################################################
 #xbmc
 ifdef CONFIG_BUILD_XBMC
-BDEPENDS_${P} += $(target_xbmc) $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad)
-opkg_my_list += libblkid1
+BDEPENDS_${P} += $(target_xbmc) $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad) $(target_tvheadend)
+opkg_my_list += libblkid1 tvheadend
 endif
 
 #########################################################################################
