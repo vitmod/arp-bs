0aa27385056c4643806f7bd9f2fb84024e77f66c
schpuntik <schpuntik@freenet.de>
2016-02-12 20:48:38 +0100

    add loadfiles for cammanager

--- make/target/neutrino-plugins/target-neutrino-plugins.mk
+++ make/target/neutrino-plugins/target-neutrino-plugins.mk
@@ -6,7 +6,7 @@ package[[ target_neutrino_plugins
 
 BDEPENDS_${P} =  $(target_lua) $(target_neutrino)
 PV_${P} = git
-PR_${P} = 2
+PR_${P} = 3
 
 DESCRIPTION_${P} = Framebuffer-based digital media application
 
@@ -37,6 +37,13 @@ rule[[
   nothing:file://flex_cammanager.conf
   nothing:file://flex_tuxcom.conf
   nothing:file://flex_tuxwetter.conf
+  nothing:file://camd3_load_*
+  nothing:file://cccam_load_*
+  nothing:file://flex_loadenigma.conf
+  nothing:file://flex_tuner.conf
+  nothing:file://mgcamd_load_*
+  nothing:file://oscam_load_*
+  nothing:file://wicardd_load_*
 ]]rule
 
 call[[ git ]]
@@ -64,11 +71,23 @@ $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 	$(run_make) -C $(DIR_${P}) install DESTDIR=$(PKDIR)
 	$(INSTALL_DIR) $(PKDIR)/etc/init.d
 	$(INSTALL_DIR) $(PKDIR)/var/tuxbox/config/flex
+	$(INSTALL_DIR) $(PKDIR)/var/tuxbox/config/flex/camd3_load
+	$(INSTALL_DIR) $(PKDIR)/var/tuxbox/config/flex/cccam_load
+	$(INSTALL_DIR) $(PKDIR)/var/tuxbox/config/flex/mgcamd_load
+	$(INSTALL_DIR) $(PKDIR)/var/tuxbox/config/flex/oscam_load
+	$(INSTALL_DIR) $(PKDIR)/var/tuxbox/config/flex/wicardd_load
 	$(INSTALL_BIN) $(DIR_${P})/cam $(PKDIR)/etc/init.d/cam
 	$(INSTALL_BIN) $(DIR_${P})/camstartstop $(PKDIR)/var/tuxbox/plugins/camstartstop
 	$(INSTALL_FILE) $(DIR_${P})/flex_cammanager.conf $(PKDIR)/var/tuxbox/config/flex/flex_cammanager.conf
+	$(INSTALL_FILE) $(DIR_${P})/flex_loadenigma.conf $(PKDIR)/var/tuxbox/config/flex/flex_loadenigma.conf
+	$(INSTALL_FILE) $(DIR_${P})/flex_tuner.conf $(PKDIR)/var/tuxbox/config/flex/flex_tuner.conf
 	$(INSTALL_FILE) $(DIR_${P})/flex_tuxcom.conf $(PKDIR)/var/tuxbox/config/flex/flex_tuxcom.conf
 	$(INSTALL_FILE) $(DIR_${P})/flex_tuxwetter.conf $(PKDIR)/var/tuxbox/config/flex/flex_tuxwetter.conf
+	$(INSTALL_FILE) $(DIR_${P})/camd3_load_* $(PKDIR)/var/tuxbox/config/flex/camd3_load/
+	$(INSTALL_FILE) $(DIR_${P})/cccam_load_* $(PKDIR)/var/tuxbox/config/flex/cccam_load/
+	$(INSTALL_FILE) $(DIR_${P})/mgcamd_load_* $(PKDIR)/var/tuxbox/config/flex/mgcamd_load/
+	$(INSTALL_FILE) $(DIR_${P})/oscam_load_* $(PKDIR)/var/tuxbox/config/flex/oscam_load/
+	$(INSTALL_FILE) $(DIR_${P})/wicardd_load_* $(PKDIR)/var/tuxbox/config/flex/wicardd_load/
 
 	touch $@
 
@@ -97,7 +116,14 @@ PACKAGES_${P} = \
 	neutrino_plugins_tank \
 	neutrino_plugins_tetris \
 	neutrino_plugins_vierg \
-	neutrino_plugins_yahtzee
+	neutrino_plugins_yahtzee \
+	neutrino_camd3_load \
+	neutrino_cccam_load \
+	neutrino_mgcamd_load \
+	neutrino_oscam_load \
+	neutrino_wicardd_load \
+	neutrino_enigma2_load \
+	neutrino_tuner_load
 
 FILES_neutrino_plugins_cammanager = /etc/init.d/cam /var/tuxbox/plugins/camstartstop /var/tuxbox/config/flex/flex_cammanager.conf
 RDEPENDS_neutrino_plugins_cammanager = neutrino_plugins_shellexec neutrino_plugins_msgbox
@@ -129,11 +155,23 @@ RDEPENDS_neutrino_plugins_sudoku = neutrino-plugins-libfx2
 FILES_neutrino_plugins_tank = /var/tuxbox/plugins/tank.*
 RDEPENDS_neutrino_plugins_tank = neutrino-plugins-libfx2
 FILES_neutrino_plugins_tetris = /var/tuxbox/plugins/tetris.*
-RDEPENDS_eutrino_plugins_tetris = neutrino-plugins-libfx2
+RDEPENDS_neutrino_plugins_tetris = neutrino-plugins-libfx2
 FILES_neutrino_plugins_vierg = /var/tuxbox/plugins/vierg.*
 RDEPENDS_neutrino_plugins_vierg = neutrino-plugins-libfx2
 FILES_neutrino_plugins_yahtzee = /var/tuxbox/plugins/yahtzee.*
 RDEPENDS_neutrino_plugins_yahtzee = neutrino-plugins-libfx2
+FILES_neutrino_camd3_load = /var/tuxbox/config/flex/camd3_load/camd3_load*
+RDEPENDS_neutrino_camd3_load = neutrino_plugins_shellexec enigma2-plugin-softcams-camd3 neutrino_plugins_cammanager
+FILES_neutrino_cccam_load = /var/tuxbox/config/flex/cccam_load/cccam_load*
+RDEPENDS_neutrino_cccam_load = neutrino_plugins_shellexec enigma2-plugin-softcams-cccam neutrino_plugins_cammanager
+FILES_neutrino_mgcamd_load = /var/tuxbox/config/flex/mgcamd_load/mgcamd_load*
+RDEPENDS_neutrino_mgcamd_load = neutrino_plugins_shellexec enigma2-plugin-softcams-mgcamd neutrino_plugins_cammanager
+FILES_neutrino_oscam_load = /var/tuxbox/config/flex/oscam_load/oscam_load*
+RDEPENDS_neutrino_oscam_load = neutrino_plugins_shellexec enigma2-plugin-softcams-oscam neutrino_plugins_cammanager
+FILES_neutrino_wicardd_load = /var/tuxbox/config/flex/wicardd_load/wicardd_load*
+RDEPENDS_neutrino_wicardd_load = neutrino_plugins_shellexec enigma2-plugin-softcams-wicardd neutrino_plugins_cammanager
+FILES_neutrino_enigma2_load = /var/tuxbox/config/flex/flex_loadenigma.conf
+FILES_neutrino_tuner_load = /var/tuxbox/config/flex/flex_tuner.conf
 
 call[[ ipkbox ]]
 
