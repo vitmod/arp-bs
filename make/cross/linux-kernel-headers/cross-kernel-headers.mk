f0b1a27db367b807124add65465582b493e530b1
Alex Maystrenko <alexeytech@gmail.com>
2016-03-01 18:19:09 +0100

    kernel headers use one version for toolchain

--- make/cross/linux-kernel-headers/cross-kernel-headers.mk
+++ make/cross/linux-kernel-headers/cross-kernel-headers.mk
@@ -8,19 +8,12 @@ package[[ target_kernel_headers
 BDEPENDS_${P} = $(target_filesystem)
 
 PR_${P} = 2
-ifeq ($(CONFIG_KERNEL_0211),y)
-PV_${P} = 2.6.32.46-47
-endif
-ifeq ($(CONFIG_KERNEL_0215),y)
 PV_${P} = 2.6.32.46-48
-endif
-ifeq ($(CONFIG_KERNEL_0217),y)
-PV_${P} = 2.6.32.46-48
-endif
+
 ${P}_SPEC = stm-target-${PN}-kbuild.spec
 ${P}_SPEC_PATCH =
 ${P}_PATCHES =
-${P}_SRCRPM = $(archivedir)/$(STLINUX)-target-linux-${PN}-$(PV_${P}).src.rpm
+${P}_SRCRPM = $(archivedir)/$(STLINUX)-target-linux-${PN}-${PV}.src.rpm
 
 call[[ base ]]
 call[[ base_rpm ]]
