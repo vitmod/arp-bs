96226dcfe659f6161205810a077f221d2c165368
schpuntik <schpuntik@freenet.de>
2016-03-24 01:08:57 +0100

    add neutrino_enigma2_load  to release

--- make/target/rootfs/target-rootfs.mk
+++ make/target/rootfs/target-rootfs.mk
@@ -225,7 +225,7 @@ endif
 
 ifdef CONFIG_BUILD_NEUTRINO
 BDEPENDS_${P} += $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad) $(target_neutrino) $(target_neutrino_plugins)
-opkg_my_list += neutrino libblkid1 neutrino-plugins-cammanager
+opkg_my_list += neutrino libblkid1 neutrino-plugins-cammanager neutrino-enigma2-load
 endif
 
 ########################################################################################
