449d09c0f1e78631623f99008280e523f11d7b8e
schpuntik <schpuntik@freenet.de>
2016-03-29 21:17:26 +0200

    luacuirl build config update

--- make/target/luacurl/target-luacurl.mk
+++ make/target/luacurl/target-luacurl.mk
@@ -6,7 +6,7 @@ package[[ target_luacurl
 BDEPENDS_${P} = $(target_glibc) $(target_lua) $(target_curl)
 
 PV_${P} = git
-PR_${P} = 1
+PR_${P} = 2
 
 call[[ base ]]
 
@@ -21,22 +21,22 @@ call[[ base_do_prepare ]]
 $(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare
 	cd $(DIR_${P}) && \
 		$(BUILDENV) \
-		DESTDIR=$(targetprefix)/ && \
-		LIBDIR=$(targetprefix)/usr/lib && \
-		LUA_INC=$(targetprefix)/usr/include && \
-		$(run_make) all $(MAKE_ARGS)
+		DESTDIR=$(targetprefix)/ \
+		LIBDIR=$(targetprefix)/usr/lib \
+		LUA_INC=$(targetprefix)/usr/include \
+		$(run_make) $(MAKE_ARGS) LUAV=$(LUA_VER_SHORT)
 	touch $@
 
 $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 	$(PKDIR_clean)
-	cd $(DIR_${P}) && $(run_make) install $(PKDIR)/usr/lib
+	cd $(DIR_${P}) && $(run_make) install LUA_CMOD=/usr/lib/lua/$(LUA_VER_SHORT) LUA_LMOD=/usr/share/lua/$(LUA_VER_SHORT) DESTDIR=$(PKDIR)/
 	touch $@
 
 call[[ ipk ]]
 
 DESCRIPTION_${P} = Lua module binding CURL
 RDEPENDS_${P} = liblua
-FILES_${P} = ##/usr/bin/lua /usr/bin/luac /usr/lib/*
+FILES_${P} = /usr/share/lua/*  /usr/lib/lua/*
 
 call[[ provides_so ]]
 call[[ ipkbox ]]
