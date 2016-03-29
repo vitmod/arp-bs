ac72fb657f5005ae1b2ef33cc6a957a6ce88d31e
schpuntik <schpuntik@freenet.de>
2016-03-29 21:16:33 +0200

    lua build update

--- make/target/lua/target-lua.mk
+++ make/target/lua/target-lua.mk
@@ -6,14 +6,18 @@ package[[ target_lua
 BDEPENDS_${P} = $(target_glibc) $(target_ncurses) $(target_curl)
 
 PV_${P} = 5.2.3
-PR_${P} = 1
+PR_${P} = 3
+
+LUA_VER = ${PV}
+LUA_VER_SHORT = 5.2
+LUAPOSIX_VER = 31
 
 call[[ base ]]
 
 rule[[
   extract:http://www.lua.org/ftp/${PN}-${PV}.tar.gz
-  git://github.com/luaposix/luaposix.git;r=release-v31
-  patch:file://liblua-${PV}-luaposix-31.patch
+  git://github.com/luaposix/luaposix.git;r=release-v$(LUAPOSIX_VER)
+  patch:file://liblua-${PV}-luaposix-$(LUAPOSIX_VER).patch
 ]]rule
 
 call[[ git ]]
@@ -23,9 +27,10 @@ call[[ base_do_prepare ]]
 $(TARGET_${P}).do_prepare_post: $(TARGET_${P}).do_prepare
 	set -e; \
 	cd $(DIR_${P})/luaposix.git/ext; cp posix/posix.c include/lua52compat.h ../../src/; cd ../..; \
+	cd $(DIR_${P})/luaposix.git/lib; cp *.lua $(DIR_${P})/; cd ../..; \
 	sed -i 's/<config.h>/"config.h"/' src/posix.c; \
 	sed -i '/^#define/d' src/lua52compat.h; \
-	sed -i 's@^#define LUA_ROOT.*@#define LUA_ROOT "/"@' src/luaconf.h; \
+	sed -i 's@^#define LUA_ROOT.*@#define LUA_ROOT "/usr/"@' src/luaconf.h; \
 	sed -i '/^#define LUA_USE_READLINE/d' src/luaconf.h; \
 	sed -i 's/ -lreadline//' src/Makefile; \
 	
@@ -34,12 +39,12 @@ $(TARGET_${P}).do_prepare_post: $(TARGET_${P}).do_prepare
 $(TARGET_${P}).do_compile: $(TARGET_${P}).do_prepare_post
 	cd $(DIR_${P}) && \
 		$(BUILDENV) \
-		$(run_make) $(MAKE_ARGS) BUILDMODE=dynamic PKG_VERSION=5.2.3 AR="$(target)-ar rcu" linux
+		$(run_make) $(MAKE_ARGS) BUILDMODE=dynamic PKG_VERSION=$(LUA_VER) AR="$(target)-ar rcu" linux
 	touch $@
 
 $(TARGET_${P}).do_package: $(TARGET_${P}).do_compile
 	$(PKDIR_clean)
-	cd $(DIR_${P}) && $(run_make) install INSTALL_TOP=$(PKDIR)/usr
+	cd $(DIR_${P}) && $(run_make) install INSTALL_TOP=$(PKDIR)/usr;  cp *.lua  $(PKDIR)/usr/share/lua/$(LUA_VER_SHORT)
 	touch $@
 
 call[[ ipk ]]
@@ -47,7 +52,7 @@ call[[ ipk ]]
 NAME_${P} = liblua
 DESCRIPTION_${P} = Lua is a powerful, fast, lightweight, embeddable scripting language
 RDEPENDS_${P} = libncurses5
-FILES_${P} = /usr/bin/lua /usr/bin/luac /usr/lib/*
+FILES_${P} = /usr/bin/lua /usr/bin/luac /usr/lib/* /usr/share/lua/*
 
 call[[ provides_so ]]
 call[[ ipkbox ]]
