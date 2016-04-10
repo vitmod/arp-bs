8ee316893ccb144e5d0df83c1677b7d00818b9e4
schpuntik <schpuntik@freenet.de>
2016-04-10 13:59:50 +0200

    add luasoap to release build

--- make/target/rootfs/target-rootfs.mk
+++ make/target/rootfs/target-rootfs.mk
@@ -232,7 +232,7 @@ endif
 #neutrino
 
 ifdef CONFIG_BUILD_NEUTRINO
-BDEPENDS_${P} += $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad) $(target_neutrino) $(target_luajson) $(target_neutrino_plugins) $(target_lua_feedparser) $(target_luacurl)
+BDEPENDS_${P} += $(target_libid3tag) $(target_libvorbisidec) $(target_libcap) $(target_libmad) $(target_neutrino) $(target_luajson) $(target_neutrino_plugins) $(target_lua_feedparser) $(target_luacurl) $(target_luasoap)
 opkg_my_list += neutrino libblkid1 neutrino-plugins-cammanager neutrino-enigma2-load lua-feedparser luasoap luajson luacurl
 ifdef CONFIG_SPARK7162
 opkg_my_list +=neutrino-tuner-load
